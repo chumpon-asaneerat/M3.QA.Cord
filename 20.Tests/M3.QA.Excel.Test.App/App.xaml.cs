b101450299770246c931a97e50d3a5da28968b21
chumpon-asaneerat <chumponsenate@gmail.com>
2023-12-08 13:37:07 +0700

    M3 QA Cord #6 - Add projects.   - Excel Test App.

---
+++ 20.Tests/M3.QA.Excel.Test.App/App.xaml.cs
@@ -0,0 +1,123 @@
+﻿using NLib.Logs;
+using NLib;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace M3.Cord.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// OnStartup.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            #region Check Current Domain/Context
+
+            Console.WriteLine("OnStartUp");
+            if (null != AppDomain.CurrentDomain)
+            {
+                if (null != System.Threading.Thread.CurrentContext)
+                {
+                    Console.WriteLine("Thread CurrentContext is not null.");
+                }
+            }
+
+            #endregion
+
+            #region Create Application Environment Options
+
+            EnvironmentOptions option = new EnvironmentOptions()
+            {
+                /* Setup Application Information */
+                AppInfo = new NAppInformation()
+                {
+                    /*  This property is required */
+                    CompanyName = AppConsts.CompanyName,
+                    /*  This property is required */
+                    ProductName = AppConsts.Application.Cord.ExcelTest.ApplicationName,
+                    /* For Application Version */
+                    Version = AppConsts.Application.Cord.ExcelTest.Version,
+                    Minor = AppConsts.Application.Cord.ExcelTest.Minor,
+                    Build = AppConsts.Application.Cord.ExcelTest.Build,
+                    LastUpdate = AppConsts.Application.Cord.ExcelTest.LastUpdate
+                },
+                /* Setup Storage */
+                Storage = new NAppStorage()
+                {
+                    StorageType = NAppFolder.Application
+                },
+                /* Setup Behaviors */
+                Behaviors = new NAppBehaviors()
+                {
+                    /* Set to true for allow only one instance of application can execute an runtime */
+                    IsSingleAppInstance = true,
+                    /* Set to true for enable Debuggers this value should always be true */
+                    EnableDebuggers = true
+                }
+            };
+
+            #endregion
+
+            #region Setup Option to Controller and check instance
+
+            WpfAppContoller.Instance.Setup(option);
+
+            if (option.Behaviors.IsSingleAppInstance &&
+                WpfAppContoller.Instance.HasMoreInstance)
+            {
+                return;
+            }
+
+            #endregion
+
+            #region Init Preload classes
+
+            ApplicationManager.Instance.Preload(() =>
+            {
+
+            });
+
+            #endregion
+
+            // Start log manager
+            LogManager.Instance.Start();
+
+            Window window = null;
+            window = new MainWindow();
+
+            if (null != window)
+            {
+                WpfAppContoller.Instance.Run(window);
+            }
+        }
+        /// <summary>
+        /// OnExit
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Shutdown log manager
+            LogManager.Instance.Shutdown();
+
+            // Wpf shutdown process required exit code.
+
+            /* If auto close the single instance must be true */
+            bool autoCloseProcess = true;
+            WpfAppContoller.Instance.Shutdown(autoCloseProcess, e.ApplicationExitCode);
+
+            base.OnExit(e);
+        }
+    }
+}
