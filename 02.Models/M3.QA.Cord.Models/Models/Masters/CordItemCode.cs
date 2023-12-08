e3426b4f3bf525625e0d1302234959f889bc8b13
chumpon-asaneerat <chumponsenate@gmail.com>
2023-12-08 15:33:33 +0700

    M3 QA Cord #12 - Update M3.QA.Excel.Test.App #4

---
+++ 02.Models/M3.QA.Cord.Models/Models/Masters/CordItemCode.cs
@@ -0,0 +1,133 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+using NLib.Models;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace M3.Cord.Models
+{
+    public class CordItemCode : NInpc
+    {
+        #region Public Proeprties
+
+        public string Item400 { get; set; }
+        public string ItemYarn { get; set; }
+        public string ItemGroup { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Save
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static NDbResult<CordItemCode> Save(CordItemCode value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<CordItemCode> ret = new NDbResult<CordItemCode>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@ItemCode", value.Item400);
+            p.Add("@ItemYarn", value.ItemYarn);
+            p.Add("@ItemGroup", value.ItemGroup);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveItemCode", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+        /// <summary>
+        /// Gets
+        /// </summary>
+        /// <returns></returns>
+        public static NDbResult<List<CordItemCode>> Gets()
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<CordItemCode>> rets = new NDbResult<List<CordItemCode>>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                rets.ErrNum = 8000;
+                rets.ErrMsg = msg;
+
+                return rets;
+            }
+
+            var p = new DynamicParameters();
+
+            try
+            {
+                var items = cnn.Query<CordItemCode>("GetItemCodes", p,
+                    commandType: CommandType.StoredProcedure);
+                var data = (null != items) ? items.ToList() : null;
+                rets.Success(data);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                rets.ErrNum = 9999;
+                rets.ErrMsg = ex.Message;
+            }
+
+            if (null == rets.data)
+            {
+                // create empty list.
+                rets.data = new List<CordItemCode>();
+            }
+
+            return rets;
+        }
+
+        #endregion
+    }
+}
---
+++ 02.Models/M3.QA.Cord.Models/Models/Masters/Customer.cs
@@ -0,0 +1,186 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+using NLib.Models;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace M3.Cord.Models
+{
+    public class Customer
+    {
+        #region Public Proeprties
+
+        public int? CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Gets
+        /// </summary>
+        /// <returns></returns>
+        public static NDbResult<List<Customer>> Gets()
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<Customer>> rets = new NDbResult<List<Customer>>();
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                rets.ErrNum = 8000;
+                rets.ErrMsg = msg;
+
+                return rets;
+            }
+
+            var p = new DynamicParameters();
+
+            try
+            {
+                var items = cnn.Query<Customer>("GetCustomers", p,
+                    commandType: CommandType.StoredProcedure);
+                var data = (null != items) ? items.ToList() : null;
+                rets.Success(data);
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                rets.ErrNum = 9999;
+                rets.ErrMsg = ex.Message;
+            }
+
+            if (null == rets.data)
+            {
+                // create empty list.
+                rets.data = new List<Customer>();
+            }
+
+            return rets;
+        }
+
+        /// <summary>
+        /// Save
+        /// </summary>
+        /// <param name="value">The Customer to save.</param>
+        /// <returns></returns>
+        public static NDbResult<Customer> Save(Customer value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<Customer> ret = new NDbResult<Customer>();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@CustomerName", value.CustomerName);
+            p.Add("@CustomerId", value.CustomerId, dbType: DbType.Int32, direction: ParameterDirection.InputOutput);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("SaveCustomer", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+                // Set PK
+                value.CustomerId = p.Get<int>("@CustomerId");
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        public static NDbResult Delete(Customer value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult ret = new NDbResult();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+            p.Add("@CustomerId", value.CustomerId);
+
+            try
+            {
+                cnn.Execute("DELETE FROM Customer WHERE CustomerId = @CustomerId", p, commandType: CommandType.Text);
+                ret.Success();
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        #endregion
+    }
+}
