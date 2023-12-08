3f92b37d4ae1cb157d9020ed7deccdaa3981edd5
chumpon-asaneerat <chumponsenate@gmail.com>
2023-12-08 15:07:15 +0700

    M3 QA Cord #11 - Update App Version

--- 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
@@ -33,8 +33,8 @@ namespace M3.Cord
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "1";
-                    public static DateTime LastUpdate = new DateTime(2023, 12, 08, 14, 00, 00);
+                    public static string Build = "11";
+                    public static DateTime LastUpdate = new DateTime(2023, 12, 08, 15, 00, 00);
                 }
             }
         }
