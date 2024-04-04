f64d2ae02af4518191a98deea480ed5e399ad30b
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-04 09:07:56 +0700

    M3 QA Cord #30 - Update Controls Styles #2

---
+++ 05.Controls/M3.QA.Controls/Pages/Cords/CordSampleTestDataPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿#region Using
+
+using M3.QA.Models;
+using NLib;
+using NLib.Data.Design;
+using NLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace M3.QA.Pages
+{
+    /// <summary>
+    /// Interaction logic for CordSampleTestDataPage.xaml
+    /// </summary>
+    public partial class CordSampleTestDataPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordSampleTestDataPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+            M3QAApp.Pages.GotoQAMainMenu();
+        }
+
+        private void cmdSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+            
+        }
+
+        #endregion
+    }
+}
