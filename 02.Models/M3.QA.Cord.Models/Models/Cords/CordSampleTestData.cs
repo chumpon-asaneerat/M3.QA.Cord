2fdee78396ea1865acde94287502982a4a613a86
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-05 08:29:09 +0700

    M3 QA Cord #38 - Implements Cord Test Data #8

--- 02.Models/M3.QA.Cord.Models/Models/Cords/CordSampleTestData.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordSampleTestData.cs
@@ -16,11 +16,15 @@ using Dapper;
 using Newtonsoft.Json;
 using System.Windows.Markup;
 using EPPlus.DataExtractor;
+using M3.QA.Models;
+using System.Windows.Media.Converters;
 
 #endregion
 
 namespace M3.QA.Models
 {
+    #region CordSampleTestData
+
     public class CordSampleTestData : NInpc
     {
         #region Public Properties
@@ -120,6 +124,10 @@ namespace M3.QA.Models
 
         #endregion
     }
+
+    #endregion
+
+    #region CordTensileStrengthProperty
 
     public class CordTensileStrengthProperty : NInpc
     {
@@ -352,6 +360,10 @@ namespace M3.QA.Models
         #endregion
     }
 
+    #endregion
+
+    #region CordTensileStrengthData
+
     public class CordTensileStrengthData : NInpc
     {
         #region Private Methods
@@ -385,6 +397,539 @@ namespace M3.QA.Models
         }
 
         public bool EnableReTest { get; set; } = true;
+
+        #endregion
+    }
+
+    #endregion
+}
+
+
+namespace M3.QA.Models2
+{
+    public abstract class CordTestProperty : NInpc
+    {
+        #region Internal Variables
+
+        private List<Func<decimal?>> _GetNs;
+        private List<Action<decimal?>> _SetNs;
+        private List<Func<decimal?>> _GetRs;
+        private List<Action<decimal?>> _SetRs;
+
+        #endregion
+
+        #region Constructor
+
+        public CordTestProperty() : base()
+        {
+            #region Init Get/Set link methods
+
+            // Get N
+            _GetNs = new List<Func<decimal?>>()
+            {
+                () => { return this.N1; },
+                () => { return this.N2; },
+                () => { return this.N3; },
+                () => { return this.N4; },
+                () => { return this.N5; },
+                () => { return this.N6; },
+                () => { return this.N7; }
+            };
+            // Set N
+            _SetNs = new List<Action<decimal?>>()
+            {
+                (value) => { this.N1 = value; },
+                (value) => { this.N2 = value; },
+                (value) => { this.N3 = value; },
+                (value) => { this.N4 = value; },
+                (value) => { this.N5 = value; },
+                (value) => { this.N6 = value; },
+                (value) => { this.N7 = value; }
+            };
+            // Get R
+            _GetRs = new List<Func<decimal?>>()
+            {
+                () => { return this.R1; },
+                () => { return this.R2; },
+                () => { return this.R3; },
+                () => { return this.R4; },
+                () => { return this.R5; },
+                () => { return this.R6; },
+                () => { return this.R7; }
+            };
+            // Set R
+            _SetNs = new List<Action<decimal?>>()
+            {
+                (value) => { this.R1 = value; },
+                (value) => { this.R2 = value; },
+                (value) => { this.R3 = value; },
+                (value) => { this.R4 = value; },
+                (value) => { this.R5 = value; },
+                (value) => { this.R6 = value; },
+                (value) => { this.R7 = value; }
+            };
+
+            #endregion
+
+            // Build default sample items.
+            BuildItems(3);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CalcAvg()
+        {
+
+        }
+
+        protected internal void BuildItems(int noOfSample)
+        {
+            Items = new List<CordTestPropertyItem>();
+            CordTestPropertyItem item;
+            for (int i = 1; i < 7; i++) 
+            {
+                if (i > noOfSample) continue; // skip if more than allow no of sample.
+
+                item = new CordTestPropertyItem();
+                item.No = i;
+                // Link get/set methods.
+                item.GetN = (null != _GetNs) ? _GetNs[i - 1] : null;
+                item.SetN = (null != _SetNs) ? _SetNs[i - 1] : null;
+                item.GetR = (null != _GetRs) ? _GetRs[i - 1] : null;
+                item.SetR = (null != _SetRs) ? _SetRs[i - 1] : null;
+
+                Items.Add(item);
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #region LotNo/SPNo
+
+        public string LotNo { get; set; }
+        public int SPNo { get; set; }
+        /// <summary>
+        /// Gets Max No of Test/Retest/
+        /// </summary>
+        public int NoOfSample { get; set; }
+
+        #endregion
+
+        #region Normal Test (1-7)
+
+        public decimal? N1
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? N2
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? N3
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? N4
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? N5
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? N6
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? N7
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+
+        /// <summary>Gets is N1 visible on UI.</summary>
+        public bool AllowN1 { get; set; }
+
+        /// <summary>Gets is R1 visible on UI.</summary>
+        public bool AllowR1 { get; set; }
+
+        #endregion
+
+        #region Re Test (1-7)
+
+        public decimal? R1
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? R2
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? R3
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? R4
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? R5
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? R6
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+        public decimal? R7
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    CalcAvg();
+                });
+            }
+        }
+
+        #endregion
+
+        #region Avg
+
+        public decimal? Avg
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    
+                });
+            }
+        }
+
+        #endregion
+
+        public List<CordTestPropertyItem> Items { get; set; }
+
+        #endregion
+    }
+
+    public class CordTestPropertyItem : NInpc 
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordTestPropertyItem() : base() { }
+
+        #endregion
+
+        #region virtual methods
+
+        protected virtual void CheckRange() { }
+
+        #endregion
+
+        #region Protected Properties
+
+        protected internal Func<decimal?> GetN { get; set; }
+        protected internal Action<decimal?> SetN { get; set; }
+
+        protected internal Func<decimal?> GetR { get; set; }
+        protected internal Action<decimal?> SetR { get; set; }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets Test No. (N1, N2, N3)
+        /// </summary>
+        public int No { get; set; }
+
+        public decimal? N
+        {
+            get 
+            { 
+                return (null != GetN) ? GetN() : new decimal?();
+            }
+            set
+            {
+                if (null != SetN)
+                {
+                    SetN(value);
+                }
+            }
+        }
+        public decimal? R
+        {
+            get 
+            {
+                return (null != GetR) ? GetR() : new decimal?();
+            }
+            set
+            {
+                if (null != SetR)
+                {
+                    SetR(value);
+                }
+            }
+        }
+
+        public bool EnableReTest { get; set; } = true;
+
+        #endregion
+    }
+
+    public class CordTensileStrengthProperty : CordTestProperty
+    {
+        #region Private Methods
+
+        #endregion
+
+        #region Public Properties
+
+        public string InputBy { get; set; }
+        public DateTime? InputDate { get; set; }
+        public string EditBy { get; set; }
+        public DateTime? EditDate { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        public static List<CordTensileStrengthProperty> Create(int maxSP)
+        {
+            List<CordTensileStrengthProperty> results = new List<CordTensileStrengthProperty>();
+            for (int i = 1; i <= maxSP; i++)
+            {
+                if (i > 7) continue;
+                var inst = new CordTensileStrengthProperty() { SPNo = i };
+
+                results.Add(inst);
+            }
+            return results;
+        }
+        /// <summary>
+        /// Gets CordSampleTestData by Lot No.
+        /// </summary>
+        /// <param name="value">The CordSampleTestData item to save.</param>
+        /// <returns></returns>
+        public static NDbResult<List<CordTensileStrengthProperty>> GetsByLotNo(string lotNo)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<CordTensileStrengthProperty>> ret = new NDbResult<List<CordTensileStrengthProperty>>();
+
+            if (string.IsNullOrWhiteSpace(lotNo))
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@LotNo", lotNo);
+
+            try
+            {
+                var items = cnn.Query<CordTensileStrengthProperty>("P_GetTensileDataByLot",
+                    p, commandType: CommandType.StoredProcedure).ToList();
+                if (null != items)
+                {
+                    // call SP M_GetPropertyTotalNByItem to gets NoSample
+                    int noOfSample = 3;
+
+                    foreach (var item in items)
+                    {
+                        item.BuildItems(noOfSample);
+                    }
+                }
+                ret.Success(items);
+                // Set error number/message
+                ret.ErrNum = 0;
+                ret.ErrMsg = "Success";
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        public static NDbResult<CordTensileStrengthProperty> Save(CordTensileStrengthProperty value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<CordTensileStrengthProperty> ret = new NDbResult<CordTensileStrengthProperty>();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@LotNo", value.LotNo);
+            p.Add("@spno", value.SPNo);
+
+            p.Add("@n1", value.N1);
+            p.Add("@n2", value.N2);
+            p.Add("@n3", value.N3);
+            p.Add("@r1", value.R1);
+            p.Add("@r2", value.R2);
+            p.Add("@r3", value.R3);
+
+            p.Add("@user", value.EditBy);
+            p.Add("@savedate", value.EditDate);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("P_SaveTensile", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
 
         #endregion
     }
