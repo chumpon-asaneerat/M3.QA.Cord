9158c2ef759fdf9e188501e8650604f35f1b6d1d
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-05 11:50:49 +0700

    M3 QA Cord #48 - Implements Cord Test Data #18

--- 02.Models/M3.QA.Cord.Models/Models/Cords/CordSampleTestData.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordSampleTestData.cs
@@ -179,6 +179,7 @@ namespace M3.QA.Models
                 item = new CordTestPropertyItem();
                 item.No = i;
                 item.SPNo = SPNo; // assign SPNo
+                item.NeedSP = NeedSP;
 
                 // Link get/set methods.
                 item.GetN = (null != _GetNs) ? _GetNs[i - 1] : null;
@@ -198,6 +199,8 @@ namespace M3.QA.Models
 
         /// <summary>Gets or sets Lot No.</summary>
         public string LotNo { get; set; }
+        /// <summary>Gets or sets Property No.</summary>
+        public int PropertyNo { get; set; }
         /// <summary>Gets or sets SP No.</summary>
         public int? SPNo { get; set; }
         /// <summary>Gets Max No of Test/Retest.</summary>
@@ -216,13 +219,16 @@ namespace M3.QA.Models
             }
         }
 
+        /// <summary>Gets or sets Need SP to Enable Test.</summary>
+        public bool NeedSP { get; set; }
+
         #endregion
 
         #region Enable Test (Normal/Re Test)
 
         public bool EnableTest
         {
-            get { return SPNo.HasValue; }
+            get { return (NeedSP) ? SPNo.HasValue : false; }
             set { }
         }
 
@@ -408,11 +414,14 @@ namespace M3.QA.Models
 
         #endregion
 
-        /// <summary>Gets is N1 visible on UI.</summary>
-        public bool AllowN1 { get; set; }
+        #region User/EditDate
 
-        /// <summary>Gets is R1 visible on UI.</summary>
-        public bool AllowR1 { get; set; }
+        public string InputBy { get; set; }
+        public DateTime? InputDate { get; set; }
+        public string EditBy { get; set; }
+        public DateTime? EditDate { get; set; }
+
+        #endregion
 
         #region Items
 
@@ -517,12 +526,14 @@ namespace M3.QA.Models
                 }
             }
         }
+        /// <summary>Gets or sets Need SP to Enable Test.</summary>
+        public bool NeedSP { get; set; }
         /// <summary>
         /// Check is Enable Normal Test. 
         /// </summary>
         public bool EnableNormalTest
         {
-            get { return SPNo.HasValue; }
+            get { return (NeedSP) ? SPNo.HasValue : false; }
             set { }
         }
         /// <summary>
@@ -530,7 +541,7 @@ namespace M3.QA.Models
         /// </summary>
         public bool EnableReTest
         {
-            get { return SPNo.HasValue && N.HasValue; } 
+            get { return (NeedSP) ? SPNo.HasValue && N.HasValue : N.HasValue; } 
             set { } 
         }
         /// <summary>Gets N Display Caption.</summary>
@@ -550,15 +561,6 @@ namespace M3.QA.Models
     /// </summary>
     public class CordTensileStrengthProperty : CordTestProperty
     {
-        #region Public Properties
-
-        public string InputBy { get; set; }
-        public DateTime? InputDate { get; set; }
-        public string EditBy { get; set; }
-        public DateTime? EditDate { get; set; }
-
-        #endregion
-
         #region Static Methods
 
         /// <summary>
@@ -598,7 +600,9 @@ namespace M3.QA.Models
                 var inst = new CordTensileStrengthProperty() 
                 { 
                     LotNo = lotNo, 
+                    PropertyNo = 1, // TensileStrength = 1
                     SPNo = SP,
+                    NeedSP = true,
                     NoOfSample = noOfSample 
                 };
 
@@ -606,17 +610,20 @@ namespace M3.QA.Models
             }
             return results;
         }
+
         /// <summary>
         /// Clone.
         /// </summary>
         /// <param name="src"></param>
         /// <param name="dst"></param>
+        /// 
         public static void Clone(CordTensileStrengthProperty src, CordTensileStrengthProperty dst)
         {
             if (null == src || null == dst)
                 return;
 
             dst.LotNo = src.LotNo;
+            dst.PropertyNo = src.PropertyNo;
             dst.SPNo = src.SPNo;
             dst.NoOfSample = src.NoOfSample;
 
@@ -661,6 +668,7 @@ namespace M3.QA.Models
             dst.Avg = src.Avg;
             */
         }
+
         /// <summary>
         /// Gets CordTensileStrengthProperty by Lot No.
         /// </summary>
@@ -785,6 +793,301 @@ namespace M3.QA.Models
 
     #endregion
 
+    #region CordElongationProperty
+
+    /// <summary>
+    /// The CordElongationProperty class.
+    /// </summary>
+    public class CordElongationProperty : CordTestProperty
+    {
+        #region Public Properties
+
+        public string ELongLoadN { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Create Break
+        /// </summary>
+        /// <param name="lotNo"></param>
+        /// <param name="maxSP"></param>
+        /// <param name="noOfSample"></param>
+        /// <param name="sp1"></param>
+        /// <param name="sp2"></param>
+        /// <param name="sp3"></param>
+        /// <param name="sp4"></param>
+        /// <param name="sp5"></param>
+        /// <param name="sp6"></param>
+        /// <param name="sp7"></param>
+        /// <returns></returns>
+        internal static List<CordElongationProperty> CreateBreak(string lotNo, int maxSP, int noOfSample,
+            int? sp1, int? sp2, int? sp3, int? sp4, int? sp5, int? sp6, int? sp7)
+        {
+            List<CordElongationProperty> results = new List<CordElongationProperty>();
+            for (int i = 1; i <= maxSP; i++)
+            {
+                if (i > 7) continue;
+
+                int? SP;
+                switch (i)
+                {
+                    case 1: SP = sp1; break;
+                    case 2: SP = sp2; break;
+                    case 3: SP = sp3; break;
+                    case 4: SP = sp4; break;
+                    case 5: SP = sp5; break;
+                    case 6: SP = sp6; break;
+                    case 7: SP = sp7; break;
+                    default: SP = new int?(); break;
+                }
+                var inst = new CordElongationProperty()
+                {
+                    LotNo = lotNo,
+                    PropertyNo = 2, // Elongation Break = 2
+                    SPNo = SP,
+                    NeedSP = false, // Elongation Break not requred SP No
+                    NoOfSample = noOfSample
+                };
+
+                results.Add(inst);
+            }
+            return results;
+        }
+        /// <summary>
+        /// Create Load
+        /// </summary>
+        /// <param name="lotNo"></param>
+        /// <param name="maxSP"></param>
+        /// <param name="noOfSample"></param>
+        /// <param name="sp1"></param>
+        /// <param name="sp2"></param>
+        /// <param name="sp3"></param>
+        /// <param name="sp4"></param>
+        /// <param name="sp5"></param>
+        /// <param name="sp6"></param>
+        /// <param name="sp7"></param>
+        /// <returns></returns>
+        internal static List<CordElongationProperty> CreateLoad(string lotNo, int maxSP, int noOfSample,
+            int? sp1, int? sp2, int? sp3, int? sp4, int? sp5, int? sp6, int? sp7)
+        {
+            List<CordElongationProperty> results = new List<CordElongationProperty>();
+            for (int i = 1; i <= maxSP; i++)
+            {
+                if (i > 7) continue;
+
+                int? SP;
+                switch (i)
+                {
+                    case 1: SP = sp1; break;
+                    case 2: SP = sp2; break;
+                    case 3: SP = sp3; break;
+                    case 4: SP = sp4; break;
+                    case 5: SP = sp5; break;
+                    case 6: SP = sp6; break;
+                    case 7: SP = sp7; break;
+                    default: SP = new int?(); break;
+                }
+                var inst = new CordElongationProperty()
+                {
+                    LotNo = lotNo,
+                    PropertyNo = 3, // Elongation Load = 3
+                    SPNo = SP,
+                    NeedSP = true, // Elongation Load not requred SP No
+                    NoOfSample = noOfSample
+                };
+
+                results.Add(inst);
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Clone.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dst"></param>
+        public static void Clone(CordElongationProperty src, CordElongationProperty dst)
+        {
+            if (null == src || null == dst)
+                return;
+
+            dst.LotNo = src.LotNo;
+            dst.PropertyNo = src.PropertyNo;
+            dst.SPNo = src.SPNo;
+            dst.NoOfSample = src.NoOfSample;
+            dst.ELongLoadN = src.ELongLoadN;
+
+            dst.EditBy = src.EditBy;
+            dst.EditDate = src.EditDate;
+            dst.InputBy = src.InputBy;
+            dst.InputDate = src.InputDate;
+
+            dst.N1 = src.N1;
+            dst.N2 = src.N2;
+            dst.N3 = src.N3;
+            dst.N4 = src.N4;
+            dst.N5 = src.N5;
+            dst.N6 = src.N6;
+            dst.N7 = src.N7;
+
+            dst.R1 = src.R1;
+            dst.R2 = src.R2;
+            dst.R3 = src.R3;
+            dst.R4 = src.R4;
+            dst.R5 = src.R5;
+            dst.R6 = src.R6;
+            dst.R7 = src.R7;
+
+            /*
+            dst.AllowN1 = src.AllowN1;
+            dst.AllowN2 = src.AllowN2;
+            dst.AllowN3 = src.AllowN3;
+            dst.AllowN4 = src.AllowN4;
+            dst.AllowN5 = src.AllowN5;
+            dst.AllowN6 = src.AllowN6;
+            dst.AllowN7 = src.AllowN7;
+
+            dst.AllowR1 = src.AllowR1;
+            dst.AllowR2 = src.AllowR2;
+            dst.AllowR3 = src.AllowR3;
+            dst.AllowR4 = src.AllowR4;
+            dst.AllowR5 = src.AllowR5;
+            dst.AllowR6 = src.AllowR6;
+            dst.AllowR7 = src.AllowR7;
+
+            dst.Avg = src.Avg;
+            */
+        }
+
+        /// <summary>
+        /// Gets CordElongationProperty by Lot No.
+        /// </summary>
+        /// <param name="lotNo">The Lot No.</param>
+        /// <param name="masterId">The MasterId.</param>
+        /// <returns></returns>
+        public static NDbResult<List<CordElongationProperty>> GetsByLotNo(string lotNo, int masterId)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<CordElongationProperty>> ret = new NDbResult<List<CordElongationProperty>>();
+
+            if (string.IsNullOrWhiteSpace(lotNo))
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@LotNo", lotNo);
+
+            try
+            {
+                var items = cnn.Query<CordElongationProperty>("P_GetElongationByLot",
+                    p, commandType: CommandType.StoredProcedure).ToList();
+                ret.Success(items);
+                // Set error number/message
+                ret.ErrNum = 0;
+                ret.ErrMsg = "Success";
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+        /// <summary>
+        /// Save.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static NDbResult<CordElongationProperty> Save(CordElongationProperty value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<CordElongationProperty> ret = new NDbResult<CordElongationProperty>();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@LotNo", value.LotNo);
+            p.Add("@propertyno", value.PropertyNo);
+            p.Add("@spno", value.SPNo);
+            
+            p.Add("@loadn", value.ELongLoadN);
+
+            p.Add("@n1", value.N1);
+            p.Add("@n2", value.N2);
+            p.Add("@n3", value.N3);
+            p.Add("@r1", value.R1);
+            p.Add("@r2", value.R2);
+            p.Add("@r3", value.R3);
+
+            p.Add("@user", value.EditBy);
+            p.Add("@savedate", value.EditDate);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("P_SaveElongation", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        #endregion
+    }
+
+    #endregion
+
     #region CordSampleTestData
 
     /// <summary>
@@ -816,8 +1119,10 @@ namespace M3.QA.Models
 
         public bool CanEditStartDate { get; set; }
 
-        // Test Properties
+        /// <summary>The Tensile Strengths Items.</summary>
         public List<CordTensileStrengthProperty> TensileStrengths { get; set; }
+        /// <summary>The Elongations Items.</summary>
+        public List<CordElongationProperty> Elongations { get; set; }
 
         #endregion
 
@@ -851,11 +1156,77 @@ namespace M3.QA.Models
             }
         }
 
+        private void InitElongations(string lotNo, int masterId, int totalSP)
+        {
+            int noOfSample = 0;
+            Utils.M_GetPropertyTotalNByItem total;
+
+            // For Elongation Break Proepty No = 2
+            total = Utils.M_GetPropertyTotalNByItem.GetByItem(masterId, 2).Value();
+            noOfSample = (null != total) ? total.NoSample : 0;
+
+            var eBreaks = CordElongationProperty.CreateBreak(lotNo, totalSP, noOfSample,
+                this.SP1, this.SP2, this.SP3, this.SP4, this.SP5, this.SP6, this.SP7);
+
+            var existBreaks = CordElongationProperty.GetsByLotNo(
+                lotNo, masterId).Value();
+
+            if (null != existBreaks && null != eBreaks)
+            {
+                int idx = -1;
+                foreach (var item in existBreaks)
+                {
+                    item.NoOfSample = noOfSample; // need to set because not return from db.
+
+                    idx = eBreaks.FindIndex((x) => { return x.SPNo == item.SPNo; });
+                    if (idx != -1)
+                    {
+                        CordElongationProperty.Clone(item, eBreaks[idx]);
+                    }
+                    idx++;
+                }
+            }
+
+            // For Elongation Load Proepty No = 3
+            total = Utils.M_GetPropertyTotalNByItem.GetByItem(masterId, 3).Value();
+            noOfSample = (null != total) ? total.NoSample : 0;
+
+            var eLoads = CordElongationProperty.CreateLoad(lotNo, totalSP, noOfSample,
+                this.SP1, this.SP2, this.SP3, this.SP4, this.SP5, this.SP6, this.SP7);
+
+            var existLoads = CordElongationProperty.GetsByLotNo(
+                lotNo, masterId).Value();
+
+            if (null != existLoads && null != eLoads)
+            {
+                int idx = -1;
+                foreach (var item in existLoads)
+                {
+                    item.NoOfSample = noOfSample; // need to set because not return from db.
+
+                    idx = eLoads.FindIndex((x) => { return x.SPNo == item.SPNo; });
+                    if (idx != -1)
+                    {
+                        CordElongationProperty.Clone(item, eLoads[idx]);
+                    }
+                    idx++;
+                }
+            }
+
+            var results = new List<CordElongationProperty>();
+            // Join 2 collections
+            if (null != existBreaks) results.AddRange(existBreaks);
+            if (null != existLoads) results.AddRange(existLoads);
+            // Sort by SP No.
+            Elongations = results.OrderBy(o => o.SPNo).ThenBy(o => o.PropertyNo).ToList();
+        }
+
         private void InitTestProperties()
         {
             if (TotalSP.HasValue && MasterId.HasValue)
             {
                 InitTensileStrengths(LotNo, MasterId.Value, TotalSP.Value);
+                InitElongations(LotNo, MasterId.Value, TotalSP.Value);
             }
         }
 
