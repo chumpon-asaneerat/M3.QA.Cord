d759832cf219820de898c10a2a7e6815a40c34a6
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-06 12:01:04 +0700

    M3 QA Cord #57 - Implements Cord Test Data #27

--- 02.Models/M3.QA.Cord.Models/Models/Cords/Properties/CordElongationProperty.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/Properties/CordElongationProperty.cs
@@ -33,6 +33,189 @@ namespace M3.QA.Models
         public string ELongLoadN { get; set; }
 
         #endregion
+
+        #region Static Methods
+
+        /// <summary>
+        /// Clone.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <param name="dst"></param>
+        public static void Clone(CordElongationSubProperty src, CordElongationSubProperty dst)
+        {
+            if (null == src || null == dst)
+                return;
+
+            dst.LotNo = src.LotNo;
+            dst.PropertyNo = src.PropertyNo;
+            dst.SPNo = src.SPNo;
+            dst.NoOfSample = src.NoOfSample;
+            dst.ELongLoadN = src.ELongLoadN;
+
+            dst.EditBy = src.EditBy;
+            dst.EditDate = src.EditDate;
+            dst.InputBy = src.InputBy;
+            dst.InputDate = src.InputDate;
+
+            dst.N1 = src.N1;
+            dst.N2 = src.N2;
+            dst.N3 = src.N3;
+            dst.N4 = src.N4;
+            dst.N5 = src.N5;
+            dst.N6 = src.N6;
+            dst.N7 = src.N7;
+
+            dst.R1 = src.R1;
+            dst.R2 = src.R2;
+            dst.R3 = src.R3;
+            dst.R4 = src.R4;
+            dst.R5 = src.R5;
+            dst.R6 = src.R6;
+            dst.R7 = src.R7;
+
+            /*
+            dst.AllowN1 = src.AllowN1;
+            dst.AllowN2 = src.AllowN2;
+            dst.AllowN3 = src.AllowN3;
+            dst.AllowN4 = src.AllowN4;
+            dst.AllowN5 = src.AllowN5;
+            dst.AllowN6 = src.AllowN6;
+            dst.AllowN7 = src.AllowN7;
+
+            dst.AllowR1 = src.AllowR1;
+            dst.AllowR2 = src.AllowR2;
+            dst.AllowR3 = src.AllowR3;
+            dst.AllowR4 = src.AllowR4;
+            dst.AllowR5 = src.AllowR5;
+            dst.AllowR6 = src.AllowR6;
+            dst.AllowR7 = src.AllowR7;
+
+            dst.Avg = src.Avg;
+            */
+        }
+
+        /// <summary>
+        /// Gets CordElongationSubProperty by Lot No.
+        /// </summary>
+        /// <param name="lotNo">The Lot No.</param>
+        /// <returns></returns>
+        public static NDbResult<List<CordElongationSubProperty>> GetsByLotNo(string lotNo)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<List<CordElongationSubProperty>> ret = new NDbResult<List<CordElongationSubProperty>>();
+
+            if (string.IsNullOrWhiteSpace(lotNo))
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@LotNo", lotNo);
+
+            try
+            {
+                var items = cnn.Query<CordElongationSubProperty>("P_GetElongationByLot",
+                    p, commandType: CommandType.StoredProcedure).ToList();
+                ret.Success(items);
+                // Set error number/message
+                ret.ErrNum = 0;
+                ret.ErrMsg = "Success";
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+        /// <summary>
+        /// Save.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static NDbResult<CordElongationSubProperty> Save(CordElongationSubProperty value)
+        {
+            MethodBase med = MethodBase.GetCurrentMethod();
+
+            NDbResult<CordElongationSubProperty> ret = new NDbResult<CordElongationSubProperty>();
+
+            if (null == value)
+            {
+                ret.ParameterIsNull();
+                return ret;
+            }
+
+            IDbConnection cnn = DbServer.Instance.Db;
+            if (null == cnn || !DbServer.Instance.Connected)
+            {
+                string msg = "Connection is null or cannot connect to database server.";
+                med.Err(msg);
+                // Set error number/message
+                ret.ErrNum = 8000;
+                ret.ErrMsg = msg;
+
+                return ret;
+            }
+
+            var p = new DynamicParameters();
+
+            p.Add("@LotNo", value.LotNo);
+            p.Add("@propertyno", value.PropertyNo);
+            p.Add("@spno", value.SPNo);
+
+            p.Add("@loadn", value.ELongLoadN);
+
+            p.Add("@n1", value.N1);
+            p.Add("@n2", value.N2);
+            p.Add("@n3", value.N3);
+            p.Add("@r1", value.R1);
+            p.Add("@r2", value.R2);
+            p.Add("@r3", value.R3);
+
+            p.Add("@user", value.EditBy);
+            p.Add("@savedate", value.EditDate);
+
+            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
+            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
+
+            try
+            {
+                cnn.Execute("P_SaveElongation", p, commandType: CommandType.StoredProcedure);
+                ret.Success(value);
+                // Set error number/message
+                ret.ErrNum = p.Get<int>("@errNum");
+                ret.ErrMsg = p.Get<string>("@errMsg");
+            }
+            catch (Exception ex)
+            {
+                med.Err(ex);
+                // Set error number/message
+                ret.ErrNum = 9999;
+                ret.ErrMsg = ex.Message;
+            }
+
+            return ret;
+        }
+
+        #endregion
     }
 
     #endregion
@@ -89,8 +272,8 @@ namespace M3.QA.Models
             results.Add(inst);
 
             // Check Exists data
-            var existBreaks = (value.MasterId.HasValue) ? GetsByLotNo(
-                value.LotNo, value.MasterId.Value).Value() : null;
+            var existBreaks = (value.MasterId.HasValue) ? CordElongationSubProperty.GetsByLotNo(
+                value.LotNo).Value() : null;
 
             if (null != existBreaks && null != results)
             {
@@ -112,185 +295,6 @@ namespace M3.QA.Models
             }
 
             return results;
-        }
-
-        /// <summary>
-        /// Clone.
-        /// </summary>
-        /// <param name="src"></param>
-        /// <param name="dst"></param>
-        public static void Clone(CordElongationBreakProperty src, CordElongationBreakProperty dst)
-        {
-            if (null == src || null == dst)
-                return;
-
-            dst.LotNo = src.LotNo;
-            dst.PropertyNo = src.PropertyNo;
-            dst.SPNo = src.SPNo;
-            dst.NoOfSample = src.NoOfSample;
-
-            dst.EditBy = src.EditBy;
-            dst.EditDate = src.EditDate;
-            dst.InputBy = src.InputBy;
-            dst.InputDate = src.InputDate;
-
-            dst.N1 = src.N1;
-            dst.N2 = src.N2;
-            dst.N3 = src.N3;
-            dst.N4 = src.N4;
-            dst.N5 = src.N5;
-            dst.N6 = src.N6;
-            dst.N7 = src.N7;
-
-            dst.R1 = src.R1;
-            dst.R2 = src.R2;
-            dst.R3 = src.R3;
-            dst.R4 = src.R4;
-            dst.R5 = src.R5;
-            dst.R6 = src.R6;
-            dst.R7 = src.R7;
-
-            /*
-            dst.AllowN1 = src.AllowN1;
-            dst.AllowN2 = src.AllowN2;
-            dst.AllowN3 = src.AllowN3;
-            dst.AllowN4 = src.AllowN4;
-            dst.AllowN5 = src.AllowN5;
-            dst.AllowN6 = src.AllowN6;
-            dst.AllowN7 = src.AllowN7;
-
-            dst.AllowR1 = src.AllowR1;
-            dst.AllowR2 = src.AllowR2;
-            dst.AllowR3 = src.AllowR3;
-            dst.AllowR4 = src.AllowR4;
-            dst.AllowR5 = src.AllowR5;
-            dst.AllowR6 = src.AllowR6;
-            dst.AllowR7 = src.AllowR7;
-
-            dst.Avg = src.Avg;
-            */
-        }
-
-        /// <summary>
-        /// Gets CordElongationProperty by Lot No.
-        /// </summary>
-        /// <param name="lotNo">The Lot No.</param>
-        /// <param name="masterId">The MasterId.</param>
-        /// <returns></returns>
-        public static NDbResult<List<CordElongationBreakProperty>> GetsByLotNo(string lotNo, int masterId)
-        {
-            MethodBase med = MethodBase.GetCurrentMethod();
-
-            NDbResult<List<CordElongationBreakProperty>> ret = new NDbResult<List<CordElongationBreakProperty>>();
-
-            if (string.IsNullOrWhiteSpace(lotNo))
-            {
-                ret.ParameterIsNull();
-                return ret;
-            }
-
-            IDbConnection cnn = DbServer.Instance.Db;
-            if (null == cnn || !DbServer.Instance.Connected)
-            {
-                string msg = "Connection is null or cannot connect to database server.";
-                med.Err(msg);
-                // Set error number/message
-                ret.ErrNum = 8000;
-                ret.ErrMsg = msg;
-
-                return ret;
-            }
-
-            var p = new DynamicParameters();
-
-            p.Add("@LotNo", lotNo);
-
-            try
-            {
-                var items = cnn.Query<CordElongationBreakProperty>("P_GetElongationByLot",
-                    p, commandType: CommandType.StoredProcedure).ToList();
-                ret.Success(items);
-                // Set error number/message
-                ret.ErrNum = 0;
-                ret.ErrMsg = "Success";
-            }
-            catch (Exception ex)
-            {
-                med.Err(ex);
-                // Set error number/message
-                ret.ErrNum = 9999;
-                ret.ErrMsg = ex.Message;
-            }
-
-            return ret;
-        }
-        /// <summary>
-        /// Save.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static NDbResult<CordElongationBreakProperty> Save(CordElongationBreakProperty value)
-        {
-            MethodBase med = MethodBase.GetCurrentMethod();
-
-            NDbResult<CordElongationBreakProperty> ret = new NDbResult<CordElongationBreakProperty>();
-
-            if (null == value)
-            {
-                ret.ParameterIsNull();
-                return ret;
-            }
-
-            IDbConnection cnn = DbServer.Instance.Db;
-            if (null == cnn || !DbServer.Instance.Connected)
-            {
-                string msg = "Connection is null or cannot connect to database server.";
-                med.Err(msg);
-                // Set error number/message
-                ret.ErrNum = 8000;
-                ret.ErrMsg = msg;
-
-                return ret;
-            }
-
-            var p = new DynamicParameters();
-
-            p.Add("@LotNo", value.LotNo);
-            p.Add("@propertyno", value.PropertyNo);
-            p.Add("@spno", value.SPNo);
-
-            p.Add("@loadn", null);
-
-            p.Add("@n1", value.N1);
-            p.Add("@n2", value.N2);
-            p.Add("@n3", value.N3);
-            p.Add("@r1", value.R1);
-            p.Add("@r2", value.R2);
-            p.Add("@r3", value.R3);
-
-            p.Add("@user", value.EditBy);
-            p.Add("@savedate", value.EditDate);
-
-            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
-            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
-
-            try
-            {
-                cnn.Execute("P_SaveElongation", p, commandType: CommandType.StoredProcedure);
-                ret.Success(value);
-                // Set error number/message
-                ret.ErrNum = p.Get<int>("@errNum");
-                ret.ErrMsg = p.Get<string>("@errMsg");
-            }
-            catch (Exception ex)
-            {
-                med.Err(ex);
-                // Set error number/message
-                ret.ErrNum = 9999;
-                ret.ErrMsg = ex.Message;
-            }
-
-            return ret;
         }
 
         #endregion
@@ -359,8 +363,8 @@ namespace M3.QA.Models
             }
 
             // Check Exists data
-            var existLoads = (value.MasterId.HasValue) ? GetsByLotNo(
-                value.LotNo, value.MasterId.Value).Value() : null;
+            var existLoads = (value.MasterId.HasValue) ? CordElongationSubProperty.GetsByLotNo(
+                value.LotNo).Value() : null;
 
             if (null != existLoads && null != results)
             {
@@ -384,186 +388,6 @@ namespace M3.QA.Models
             }
 
             return results;
-        }
-
-        /// <summary>
-        /// Clone.
-        /// </summary>
-        /// <param name="src"></param>
-        /// <param name="dst"></param>
-        public static void Clone(CordElongationLoadProperty src, CordElongationLoadProperty dst)
-        {
-            if (null == src || null == dst)
-                return;
-
-            dst.LotNo = src.LotNo;
-            dst.PropertyNo = src.PropertyNo;
-            dst.SPNo = src.SPNo;
-            dst.NoOfSample = src.NoOfSample;
-            dst.ELongLoadN = src.ELongLoadN;
-
-            dst.EditBy = src.EditBy;
-            dst.EditDate = src.EditDate;
-            dst.InputBy = src.InputBy;
-            dst.InputDate = src.InputDate;
-
-            dst.N1 = src.N1;
-            dst.N2 = src.N2;
-            dst.N3 = src.N3;
-            dst.N4 = src.N4;
-            dst.N5 = src.N5;
-            dst.N6 = src.N6;
-            dst.N7 = src.N7;
-
-            dst.R1 = src.R1;
-            dst.R2 = src.R2;
-            dst.R3 = src.R3;
-            dst.R4 = src.R4;
-            dst.R5 = src.R5;
-            dst.R6 = src.R6;
-            dst.R7 = src.R7;
-
-            /*
-            dst.AllowN1 = src.AllowN1;
-            dst.AllowN2 = src.AllowN2;
-            dst.AllowN3 = src.AllowN3;
-            dst.AllowN4 = src.AllowN4;
-            dst.AllowN5 = src.AllowN5;
-            dst.AllowN6 = src.AllowN6;
-            dst.AllowN7 = src.AllowN7;
-
-            dst.AllowR1 = src.AllowR1;
-            dst.AllowR2 = src.AllowR2;
-            dst.AllowR3 = src.AllowR3;
-            dst.AllowR4 = src.AllowR4;
-            dst.AllowR5 = src.AllowR5;
-            dst.AllowR6 = src.AllowR6;
-            dst.AllowR7 = src.AllowR7;
-
-            dst.Avg = src.Avg;
-            */
-        }
-
-        /// <summary>
-        /// Gets CordElongationLoadProperty by Lot No.
-        /// </summary>
-        /// <param name="lotNo">The Lot No.</param>
-        /// <param name="masterId">The MasterId.</param>
-        /// <returns></returns>
-        public static NDbResult<List<CordElongationLoadProperty>> GetsByLotNo(string lotNo, int masterId)
-        {
-            MethodBase med = MethodBase.GetCurrentMethod();
-
-            NDbResult<List<CordElongationLoadProperty>> ret = new NDbResult<List<CordElongationLoadProperty>>();
-
-            if (string.IsNullOrWhiteSpace(lotNo))
-            {
-                ret.ParameterIsNull();
-                return ret;
-            }
-
-            IDbConnection cnn = DbServer.Instance.Db;
-            if (null == cnn || !DbServer.Instance.Connected)
-            {
-                string msg = "Connection is null or cannot connect to database server.";
-                med.Err(msg);
-                // Set error number/message
-                ret.ErrNum = 8000;
-                ret.ErrMsg = msg;
-
-                return ret;
-            }
-
-            var p = new DynamicParameters();
-
-            p.Add("@LotNo", lotNo);
-
-            try
-            {
-                var items = cnn.Query<CordElongationLoadProperty>("P_GetElongationByLot",
-                    p, commandType: CommandType.StoredProcedure).ToList();
-                ret.Success(items);
-                // Set error number/message
-                ret.ErrNum = 0;
-                ret.ErrMsg = "Success";
-            }
-            catch (Exception ex)
-            {
-                med.Err(ex);
-                // Set error number/message
-                ret.ErrNum = 9999;
-                ret.ErrMsg = ex.Message;
-            }
-
-            return ret;
-        }
-        /// <summary>
-        /// Save.
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        public static NDbResult<CordElongationLoadProperty> Save(CordElongationLoadProperty value)
-        {
-            MethodBase med = MethodBase.GetCurrentMethod();
-
-            NDbResult<CordElongationLoadProperty> ret = new NDbResult<CordElongationLoadProperty>();
-
-            if (null == value)
-            {
-                ret.ParameterIsNull();
-                return ret;
-            }
-
-            IDbConnection cnn = DbServer.Instance.Db;
-            if (null == cnn || !DbServer.Instance.Connected)
-            {
-                string msg = "Connection is null or cannot connect to database server.";
-                med.Err(msg);
-                // Set error number/message
-                ret.ErrNum = 8000;
-                ret.ErrMsg = msg;
-
-                return ret;
-            }
-
-            var p = new DynamicParameters();
-
-            p.Add("@LotNo", value.LotNo);
-            p.Add("@propertyno", value.PropertyNo);
-            p.Add("@spno", value.SPNo);
-
-            p.Add("@loadn", value.ELongLoadN);
-
-            p.Add("@n1", value.N1);
-            p.Add("@n2", value.N2);
-            p.Add("@n3", value.N3);
-            p.Add("@r1", value.R1);
-            p.Add("@r2", value.R2);
-            p.Add("@r3", value.R3);
-
-            p.Add("@user", value.EditBy);
-            p.Add("@savedate", value.EditDate);
-
-            p.Add("@errNum", dbType: DbType.Int32, direction: ParameterDirection.Output);
-            p.Add("@errMsg", dbType: DbType.String, direction: ParameterDirection.Output, size: -1);
-
-            try
-            {
-                cnn.Execute("P_SaveElongation", p, commandType: CommandType.StoredProcedure);
-                ret.Success(value);
-                // Set error number/message
-                ret.ErrNum = p.Get<int>("@errNum");
-                ret.ErrMsg = p.Get<string>("@errMsg");
-            }
-            catch (Exception ex)
-            {
-                med.Err(ex);
-                // Set error number/message
-                ret.ErrNum = 9999;
-                ret.ErrMsg = ex.Message;
-            }
-
-            return ret;
         }
 
         #endregion
