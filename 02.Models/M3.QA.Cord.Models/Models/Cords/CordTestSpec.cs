dbc50d3b6f9631129a0f149b8a469767a9127769
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-16 20:14:11 +0700

    M3 QA Cord #133 - Implements Cord Test Data #103   - Add CordTestSpec class.

---
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordTestSpec.cs
@@ -0,0 +1,44 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+using Dapper;
+
+using NLib;
+using NLib.Models;
+
+#endregion
+
+namespace M3.QA.Models
+{
+    #region CordTestSpec
+
+    /// <summary>
+    /// The Cord Test Spec class.
+    /// </summary>
+    public class CordTestSpec : NInpc
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordTestSpec() : base()
+        {
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+
+    #endregion
+}
