a5d696147f1b436f6200efc8eb825419a913e4ad
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-16 20:18:01 +0700

    M3 QA Cord #134 - Implements Cord Test Data #104   - Update CordTestSpec class #2.

--- 02.Models/M3.QA.Cord.Models/Models/Cords/CordTestSpec.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordTestSpec.cs
@@ -37,6 +37,52 @@ namespace M3.QA.Models
 
         #region Public Properties
 
+        /// <summary>Gets or sets Product Code.</summary>
+        public string ProductCode 
+        {
+            get { return Get<string>(); }
+            set { Set(value, () => { }); }
+        }
+        /// <summary>Gets or sets MasterId.</summary>
+        public int? MasterId
+        {
+            get { return Get<int?>(); }
+            set { Set(value, () => { }); }
+        }
+        /// <summary>Gets or sets PropertyNo.</summary>
+        public int PropertyNo
+        {
+            get { return Get<int>(); }
+            set { Set(value, () => { }); }
+        }
+
+
+        /// <summary>Gets or sets SpecId.</summary>
+        public int SpecId
+        {
+            get { return Get<int>(); }
+            set { Set(value, () => { }); }
+        }
+        /// <summary>Gets or sets Spec description.</summary>
+        public string SpecDesc
+        {
+            get { return Get<string>(); }
+            set { Set(value, () => { }); }
+        }
+
+        /// <summary>Gets or sets UnitId.</summary>
+        public int UnitId
+        {
+            get { return Get<int>(); }
+            set { Set(value, () => { }); }
+        }
+        /// <summary>Gets or sets Unit description.</summary>
+        public string UnitDesc
+        {
+            get { return Get<string>(); }
+            set { Set(value, () => { }); }
+        }
+
         #endregion
     }
 
