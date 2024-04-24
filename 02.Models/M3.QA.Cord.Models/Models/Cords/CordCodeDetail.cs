3bcb5f80a671fe20e5ddc3e9eeef2414415f1b92
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-24 08:31:31 +0700

    M3 QA Cord #168 - Implements Cord Test Data #138

---
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordCodeDetail.cs
@@ -0,0 +1,35 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using Dapper;
+
+using NLib;
+using NLib.Models;
+
+#endregion
+
+namespace M3.QA.Models
+{
+    #region CordCodeDetail
+
+    /// <summary>
+    /// The Cord Code Detail class.
+    /// </summary>
+    public class CordCodeDetail
+    {
+        #region Public Properties
+
+        #endregion
+
+        #region Static Methods
+
+        #endregion
+    }
+
+    #endregion
+}
---
+++ 02.Models/M3.QA.Cord.Models/Models/Masters/YarnType.cs
@@ -0,0 +1,42 @@
+﻿#region Using
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace M3.QA.Models
+{
+    #region YarnType
+
+    /// <summary>
+    /// The Yarn Type class.
+    /// </summary>
+    public class YarnType
+    {
+        #region Public Properties
+
+        /// <summary>Gets or set Id.</summary>
+        public string Id { get; set; }
+        /// <summary>Gets or set Text.</summary>
+        public string Text { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        public static List<YarnType> Gets()
+        {
+            var ret = new List<YarnType>()
+            {
+                new YarnType() { Id = "Polyester", Text = "Polyester" },
+                new YarnType() { Id = "Nylon", Text = "Nylon" }
+            };
+
+            return ret;
+        }
+
+        #endregion
+    }
+
+    #endregion
+}
