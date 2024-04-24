f0cf000d47c1dd498c226bee7cc01270ec3a0a51
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-24 08:22:55 +0700

    M3 QA Cord #166 - Implements Cord Test Data #136

---
+++ 02.Models/M3.QA.Cord.Models/Models/Masters/ProductType.cs
@@ -0,0 +1,46 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using System.Windows.Media;
+
+using NLib;
+using NLib.Models;
+
+using Dapper;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace M3.QA.Models
+{
+    public class ProductType
+    {
+        #region Public Properties
+
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        public static List<ProductType> Gets()
+        {
+            var ret = new List<ProductType>() 
+            { 
+                new ProductType() { Id = "Twist", Text = "Twist" },
+                new ProductType() { Id = "Dip", Text = "Dip" }
+            };
+
+            return ret;
+        }
+
+        #endregion
+    }
+}
