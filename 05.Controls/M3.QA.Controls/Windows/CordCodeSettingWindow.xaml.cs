12f39e0c0e54d642647ec6190428acbecd720138
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-26 03:36:20 +0700

    M3 QA Cord #191 - Implements Cord Test Data #151   - Implements Cord Code Setting #5

---
+++ 05.Controls/M3.QA.Controls/Windows/CordCodeSettingWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+using NLib.Models;
+using M3.QA.Models;
+
+#endregion
+
+namespace M3.QA.Windows
+{
+    /// <summary>
+    /// Interaction logic for CordCodeSettingWindow.xaml
+    /// </summary>
+    public partial class CordCodeSettingWindow : Window
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordCodeSettingWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        private CordCodeDetail item;
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void LoadComboboxes()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup(CordCodeDetail inst)
+        {
+            this.DataContext = null;
+            LoadComboboxes();
+
+            item = inst;
+            if (null != item) 
+            { 
+
+            }
+
+            this.DataContext = item;
+        }
+
+        #endregion
+    }
+}
