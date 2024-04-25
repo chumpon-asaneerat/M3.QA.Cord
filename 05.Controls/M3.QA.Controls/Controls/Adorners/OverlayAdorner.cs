7da29a4daa00a5f086bf6cdf9cfbb0da2dd367f0
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-26 01:28:25 +0700

    M3 QA Cord #175 - Implements Cord Test Data #145

--- 05.Controls/M3.QA.Controls/Controls/Adorners/OverlayAdorner.cs
+++ 05.Controls/M3.QA.Controls/Controls/Adorners/OverlayAdorner.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using System.Windows.Documents;
 using System.Windows.Media;
 using System.Windows;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Reflection.Emit;
 
 #endregion
 
@@ -35,27 +38,48 @@ namespace M3.QA.Controls
 
         #endregion
 
+        #region Static Fields
+
+        private static CultureInfo culture = new CultureInfo("en-US");
+        private static FlowDirection flow = FlowDirection.LeftToRight;
+        private static Typeface wingdings2 = new Typeface("Wingdings 2");
+        private static double fontSize = 20;
+        private static double pixelPerDip = 100;
+
+        #endregion
+
+        #region Private Methods
+
+        private void DrawText(DrawingContext drawingContext, string text, SolidColorBrush brush, Point pt)
+        {
+            if (null == drawingContext)
+                return;
+            FormattedText fmt = new FormattedText(text, culture, flow, wingdings2, fontSize, brush, pixelPerDip);
+            drawingContext.DrawText(fmt, pt);
+        }
+
+        #endregion
+
         #region Override Methods
 
         // A common way to implement an adorner's rendering behavior is to override the OnRender
         // method, which is called by the layout system as part of a rendering pass.
         protected override void OnRender(DrawingContext drawingContext)
         {
-            /*
+            var ctrl = (null != this.AdornedElement && this.AdornedElement is FrameworkElement) ? 
+                (FrameworkElement)this.AdornedElement : null;
+            if (null == ctrl || ctrl.Visibility != Visibility.Visible)
+                return; // Not visible so skip.
+
+            var item = (null != ctrl && null != ctrl.DataContext) ?
+                ctrl.DataContext as Models.NRTestPropertyItem : null;
+
+            if (null == item) return;
+
             Rect adornedElementRect = new Rect(this.AdornedElement.DesiredSize);
-
-            // Some arbitrary drawing implements.
-            SolidColorBrush renderBrush = new SolidColorBrush(Colors.Green);
-            renderBrush.Opacity = 0.2;
-            Pen renderPen = new Pen(new SolidColorBrush(Colors.Navy), 1.5);
-            double renderRadius = 5.0;
-
-            // Draw a circle at each corner.
-            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopLeft, renderRadius, renderRadius);
-            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.TopRight, renderRadius, renderRadius);
-            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomLeft, renderRadius, renderRadius);
-            drawingContext.DrawEllipse(renderBrush, renderPen, adornedElementRect.BottomRight, renderRadius, renderRadius);
-            */
+            Point pt = new Point(adornedElementRect.Right - 20, adornedElementRect.Top);
+            
+            this.DrawText(drawingContext, "P", Brushes.ForestGreen, pt);
         }
 
         #endregion
