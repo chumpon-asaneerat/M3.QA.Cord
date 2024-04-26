906e32ce86999720f18cc21d0045e11ea48e5c49
chumpon-asaneerat <chumponsenate@gmail.com>
2024-04-26 12:57:16 +0700

    M3 QA Cord #193 - Implements Cord Test Data #153   - Implements Cord Code Setting #7   - Prepare SolutionSampleTestDataPage

---
+++ 05.Controls/M3.QA.Controls/Pages/SolutionSampleTestDataPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace M3.QA.Pages
+{
+    /// <summary>
+    /// Interaction logic for SolutionSampleTestDataPage.xaml
+    /// </summary>
+    public partial class SolutionSampleTestDataPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SolutionSampleTestDataPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void cmdCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+
+        }
+
+        #endregion
+    }
+}
