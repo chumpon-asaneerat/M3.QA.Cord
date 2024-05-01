44247b0a4a1290c155adade363adba0bf4bb11ca
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-01 23:48:17 +0700

    M3 QA Cord #243 - Implements Cord Test Data #203   - Implements DIP Solution Test Data #15

--- 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
@@ -235,6 +235,95 @@ namespace M3.QA.Models
 
         #endregion
 
+        #region Create
+
+        /*
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="totalN"></param>
+        /// <returns></returns>
+        internal static List<CordAdhesionForce> Create(CordSampleTestData value,
+            Utils.M_GetPropertyTotalNByItem totalN)
+        {
+            List<CordAdhesionForce> results = new List<CordAdhesionForce>();
+            if (null == value)
+                return results;
+
+            // For Adhesion Force Proepty No = 4
+            int noOfSample = (null != totalN) ? totalN.NoSample : 0;
+            int alllowSP = (value.TotalSP.HasValue) ? value.TotalSP.Value : 0;
+
+            // Adhesion Force Proepty No = 4
+            var spec = value.Specs.FindByPropertyNo(4);
+
+            int i = 1;
+            int iMaxLimitSP = 7;
+            while (i <= iMaxLimitSP)
+            {
+                if (results.Count >= alllowSP)
+                    break; // already reach max allow SP
+
+                int? SP;
+                switch (i)
+                {
+                    case 1: SP = value.SP1; break;
+                    case 2: SP = value.SP2; break;
+                    case 3: SP = value.SP3; break;
+                    case 4: SP = value.SP4; break;
+                    case 5: SP = value.SP5; break;
+                    case 6: SP = value.SP6; break;
+                    case 7: SP = value.SP7; break;
+                    default: SP = new int?(); break;
+                }
+                // Skip SP is null
+                if (!SP.HasValue)
+                {
+                    i++; // increase index and skip to next loop.
+                    continue;
+                }
+
+                var inst = new CordAdhesionForce()
+                {
+                    LotNo = value.LotNo,
+                    PropertyNo = 4, // Adhesion Force = 4
+                    SPNo = SP,
+                    NeedSP = true,
+                    Spec = spec,
+                    YarnType = value.YarnType,
+                    NoOfSample = noOfSample
+                };
+
+                results.Add(inst);
+
+                i++; // increase index
+            }
+
+            var existItems = (value.MasterId.HasValue) ? GetsByLotNo(value.LotNo).Value() : null;
+            if (null != existItems && null != results)
+            {
+                int idx = -1;
+                // loop trought all initial results and fill data with the exists on database
+                foreach (var item in results)
+                {
+                    idx = existItems.FindIndex((x) => { return x.SPNo == item.SPNo; });
+                    if (idx != -1)
+                    {
+                        // need to set because not return from db.
+                        existItems[idx].NoOfSample = item.NoOfSample;
+                        existItems[idx].YarnType = item.YarnType;
+                        existItems[idx].Spec = spec; // assign spec
+                        // Clone anther properties
+                        Clone(existItems[idx], item);
+                    }
+                }
+            }
+
+            return results;
+        }
+        */
+        #endregion
 
         #endregion
     }
