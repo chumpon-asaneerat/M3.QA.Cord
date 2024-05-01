d66fdae5ecd813a8d6c23a67282d8e5a4b6adb96
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-01 23:50:42 +0700

    M3 QA Cord #244 - Implements Cord Test Data #204   - Implements DIP Solution Test Data #16

--- 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionTSC.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionTSC.cs
@@ -23,42 +23,172 @@ namespace M3.QA.Models
     /// <summary>
     /// The DIP Solution TSC class.
     /// </summary>
-    public class DIPSolutionTSC : NRTestProperty
+    public class DIPSolutionTSC : NInpc
     {
-        #region Protected Methods
+        #region Constructor
 
         /// <summary>
-        /// Check Spec.
+        /// Constructor.
         /// </summary>
-        protected override void CheckSpec()
+        public DIPSolutionTSC() : base()
         {
-            if (null == Spec || Spec.SpecId <= 0)
-                return;
+            BreakerWeight = new NRTestProperty();
+            // Set calc formula
+            BreakerWeight.ValueChanges = CalculateFormula;
 
-            this.NOut1 = (N1.HasValue) ? Spec.IsOutOfSpec(N1.Value) : false;
-            this.NOut2 = (N2.HasValue) ? Spec.IsOutOfSpec(N2.Value) : false;
-            this.NOut3 = (N3.HasValue) ? Spec.IsOutOfSpec(N3.Value) : false;
-            this.NOut4 = (N4.HasValue) ? Spec.IsOutOfSpec(N4.Value) : false;
-            this.NOut5 = (N5.HasValue) ? Spec.IsOutOfSpec(N5.Value) : false;
-            this.NOut6 = (N6.HasValue) ? Spec.IsOutOfSpec(N6.Value) : false;
-            this.NOut7 = (N7.HasValue) ? Spec.IsOutOfSpec(N7.Value) : false;
+            BreakerWeightBeforeHeat = new NRTestProperty();
+            // Set calc formula
+            BreakerWeight.ValueChanges = CalculateFormula;
 
-            this.ROut1 = (R1.HasValue) ? Spec.IsOutOfSpec(R1.Value) : false;
-            this.ROut2 = (R2.HasValue) ? Spec.IsOutOfSpec(R2.Value) : false;
-            this.ROut3 = (R3.HasValue) ? Spec.IsOutOfSpec(R3.Value) : false;
-            this.ROut4 = (R4.HasValue) ? Spec.IsOutOfSpec(R4.Value) : false;
-            this.ROut5 = (R5.HasValue) ? Spec.IsOutOfSpec(R5.Value) : false;
-            this.ROut6 = (R6.HasValue) ? Spec.IsOutOfSpec(R6.Value) : false;
-            this.ROut7 = (R7.HasValue) ? Spec.IsOutOfSpec(R7.Value) : false;
+            BreakerWeightAfterHeat = new NRTestProperty();
+            // Set calc formula
+            BreakerWeight.ValueChanges = CalculateFormula;
 
-            // Raise items events
-            this.RaiseNOutChanges();
-            this.RaiseROutChanges();
+            RPU = new NRTestProperty();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CalculateFormula()
+        {
+
+        }
+
+        private void UpdateProperties()
+        {
+            if (null == BreakerWeight) BreakerWeight = new NRTestProperty();
+
+            BreakerWeight.LotNo = LotNo;
+            BreakerWeight.PropertyNo = PropertyNo;
+            BreakerWeight.NoOfSample = NoOfSample;
+            BreakerWeight.NeedSP = NeedSP;
+            // Check calculate action
+            if (null == BreakerWeight.ValueChanges)
+            {
+                BreakerWeight.ValueChanges = CalculateFormula;
+            }
+
+            if (null == BreakerWeightBeforeHeat) BreakerWeightBeforeHeat = new NRTestProperty();
+            BreakerWeightBeforeHeat.LotNo = LotNo;
+            BreakerWeightBeforeHeat.PropertyNo = PropertyNo;
+            BreakerWeightBeforeHeat.NoOfSample = NoOfSample;
+            BreakerWeightBeforeHeat.NeedSP = NeedSP;
+            // Check calculate action
+            if (null == BreakerWeightBeforeHeat.ValueChanges)
+            {
+                BreakerWeightBeforeHeat.ValueChanges = CalculateFormula;
+            }
+
+            if (null == BreakerWeightAfterHeat) BreakerWeightAfterHeat = new NRTestProperty();
+            BreakerWeightAfterHeat.LotNo = LotNo;
+            BreakerWeightAfterHeat.PropertyNo = PropertyNo;
+            BreakerWeightAfterHeat.NoOfSample = NoOfSample;
+            BreakerWeightAfterHeat.NeedSP = NeedSP;
+            // Check calculate action
+            if (null == BreakerWeightAfterHeat.ValueChanges)
+            {
+                BreakerWeightAfterHeat.ValueChanges = CalculateFormula;
+            }
+
+            if (null == RPU) RPU = new NRTestProperty();
+            RPU.LotNo = LotNo;
+            RPU.PropertyNo = PropertyNo;
+            RPU.NoOfSample = NoOfSample;
+            RPU.NeedSP = NeedSP;
+
+            CalculateFormula(); // calculate
+
+            this.Raise(() => this.EnableTest);
         }
 
         #endregion
 
         #region Public Properties
+
+        #region LotNo/PropertyNo/NoOfSample
+
+        /// <summary>Gets or sets Lot No.</summary>
+        public string LotNo
+        {
+            get { return Get<string>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+        /// <summary>Gets or sets Property No.</summary>
+        public int PropertyNo
+        {
+            get { return Get<int>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+        /// <summary>Gets Max No of Test/Retest.</summary>
+        public int NoOfSample
+        {
+            get { return Get<int>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+        /// <summary>Gets or sets Need SP to Enable Test.</summary>
+        public bool NeedSP
+        {
+            get { return Get<bool>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+
+        #endregion
+
+        #region Spec
+
+        /// <summary>Gets or sets CordTestSpec.</summary>
+        public CordTestSpec Spec { get; set; }
+
+        #endregion
+
+        #region Enable Test (Normal/Re Test)
+
+        public bool EnableTest
+        {
+            get { return true; }
+            set { }
+        }
+
+        #endregion
+
+        #region BreakerWeight/BreakerWeightBeforeHeat/BreakerWeightAfterHeat/RPU
+
+        /// <summary>Gets or sets BreakerWeight.</summary>
+        public NRTestProperty BreakerWeight { get; set; }
+        /// <summary>Gets or sets BreakerWeightBeforeHeat.</summary>
+        public NRTestProperty BreakerWeightBeforeHeat { get; set; }
+        /// <summary>Gets or sets BreakerWeightAfterHeat.</summary>
+        public NRTestProperty BreakerWeightAfterHeat { get; set; }
+        /// <summary>Gets or sets RPU.</summary>
+        public NRTestProperty RPU { get; set; }
+
+        #endregion
 
         #region User/EditDate
 
@@ -80,36 +210,119 @@ namespace M3.QA.Models
         /// </summary>
         /// <param name="src"></param>
         /// <param name="dst"></param>
-        public static void Clone(CordThickness src, CordThickness dst)
+        public static void Clone(DIPSolutionTSC src, DIPSolutionTSC dst)
         {
             if (null == src || null == dst)
                 return;
 
             dst.LotNo = src.LotNo;
             dst.PropertyNo = src.PropertyNo;
-            dst.SPNo = src.SPNo;
             dst.NoOfSample = src.NoOfSample;
-            dst.YarnType = src.YarnType;
+            dst.NeedSP = src.NeedSP;
+
+            dst.Spec = src.Spec;
 
             dst.EditBy = src.EditBy;
             dst.EditDate = src.EditDate;
             dst.InputBy = src.InputBy;
             dst.InputDate = src.InputDate;
 
-            if (null != dst.Items && null != src.Items)
+            NRTestProperty.Clone(src.BreakerWeight, dst.BreakerWeight);
+            NRTestProperty.Clone(src.BreakerWeightBeforeHeat, dst.BreakerWeightBeforeHeat);
+            NRTestProperty.Clone(src.BreakerWeightAfterHeat, dst.BreakerWeightAfterHeat);
+            NRTestProperty.Clone(src.RPU, dst.RPU);
+        }
+
+        #endregion
+
+        #region Create
+
+        /*
+        /// <summary>
+        /// Create
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="totalN"></param>
+        /// <returns></returns>
+        internal static List<CordAdhesionForce> Create(CordSampleTestData value,
+            Utils.M_GetPropertyTotalNByItem totalN)
+        {
+            List<CordAdhesionForce> results = new List<CordAdhesionForce>();
+            if (null == value)
+                return results;
+
+            // For Adhesion Force Proepty No = 4
+            int noOfSample = (null != totalN) ? totalN.NoSample : 0;
+            int alllowSP = (value.TotalSP.HasValue) ? value.TotalSP.Value : 0;
+
+            // Adhesion Force Proepty No = 4
+            var spec = value.Specs.FindByPropertyNo(4);
+
+            int i = 1;
+            int iMaxLimitSP = 7;
+            while (i <= iMaxLimitSP)
             {
-                int iMax = (dst.Items.Count > src.Items.Count) ? dst.Items.Count : src.Items.Count;
-                for (int i = 0; i < iMax; i++)
+                if (results.Count >= alllowSP)
+                    break; // already reach max allow SP
+
+                int? SP;
+                switch (i)
                 {
-                    if (i < dst.Items.Count && i < src.Items.Count)
+                    case 1: SP = value.SP1; break;
+                    case 2: SP = value.SP2; break;
+                    case 3: SP = value.SP3; break;
+                    case 4: SP = value.SP4; break;
+                    case 5: SP = value.SP5; break;
+                    case 6: SP = value.SP6; break;
+                    case 7: SP = value.SP7; break;
+                    default: SP = new int?(); break;
+                }
+                // Skip SP is null
+                if (!SP.HasValue)
+                {
+                    i++; // increase index and skip to next loop.
+                    continue;
+                }
+
+                var inst = new CordAdhesionForce()
+                {
+                    LotNo = value.LotNo,
+                    PropertyNo = 4, // Adhesion Force = 4
+                    SPNo = SP,
+                    NeedSP = true,
+                    Spec = spec,
+                    YarnType = value.YarnType,
+                    NoOfSample = noOfSample
+                };
+
+                results.Add(inst);
+
+                i++; // increase index
+            }
+
+            var existItems = (value.MasterId.HasValue) ? GetsByLotNo(value.LotNo).Value() : null;
+            if (null != existItems && null != results)
+            {
+                int idx = -1;
+                // loop trought all initial results and fill data with the exists on database
+                foreach (var item in results)
+                {
+                    idx = existItems.FindIndex((x) => { return x.SPNo == item.SPNo; });
+                    if (idx != -1)
                     {
-                        dst.Items[i].N = src.Items[i].N;
-                        dst.Items[i].R = src.Items[i].R;
+                        // need to set because not return from db.
+                        existItems[idx].NoOfSample = item.NoOfSample;
+                        existItems[idx].YarnType = item.YarnType;
+                        existItems[idx].Spec = spec; // assign spec
+                        // Clone anther properties
+                        Clone(existItems[idx], item);
                     }
                 }
             }
-        }
 
+            return results;
+        }
+        */
         #endregion
 
         #endregion
