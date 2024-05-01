5618053f2e21dfaa391861da364acb4faba1a7c7
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-01 23:34:26 +0700

    M3 QA Cord #239 - Implements Cord Test Data #199   - Implements DIP Solution Test Data #11

---
+++ 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
@@ -0,0 +1,32 @@
+﻿#region Using
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Media;
+using Dapper;
+
+using NLib;
+using NLib.Models;
+
+#endregion
+
+namespace M3.QA.Models
+{
+    #region DIPSolutionBreaker
+
+    /// <summary>
+    /// The DIP Solution Breaker class.
+    /// </summary>
+    public class DIPSolutionBreaker : NInpc
+    {
+
+    }
+
+    #endregion
+}
