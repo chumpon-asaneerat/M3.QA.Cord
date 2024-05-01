76f5fbbbf7f2ec2db195663102db9c49835e45ed
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-01 23:44:54 +0700

    M3 QA Cord #241 - Implements Cord Test Data #201   - Implements DIP Solution Test Data #13

--- 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
@@ -43,6 +43,8 @@ namespace M3.QA.Models
             BreakerWeightAfterHeat = new NRTestProperty();
             // Set calc formula
             BreakerWeight.ValueChanges = CalculateFormula;
+
+            RPU = new NRTestProperty();
         }
 
         #endregion
@@ -54,11 +56,128 @@ namespace M3.QA.Models
 
         }
 
+        private void UpdateProperties()
+        {
+            if (null == BreakerWeight) BreakerWeight = new NRTestProperty();
+
+            BreakerWeight.LotNo = LotNo;
+            BreakerWeight.PropertyNo = PropertyNo;
+            BreakerWeight.NoOfSample = NoOfSample;
+            BreakerWeight.NeedSP = NeedSP;
+            // Check calculate action
+            if (null == BreakerWeight.ValueChanges)
+            {
+                BreakerWeight.ValueChanges = CalculateFormula;
+            }
+
+            if (null == BreakerWeightBeforeHeat) BreakerWeightBeforeHeat = new NRTestProperty();
+            BreakerWeightBeforeHeat.LotNo = LotNo;
+            BreakerWeightBeforeHeat.PropertyNo = PropertyNo;
+            BreakerWeightBeforeHeat.NoOfSample = NoOfSample;
+            BreakerWeightBeforeHeat.NeedSP = NeedSP;
+            // Check calculate action
+            if (null == BreakerWeightBeforeHeat.ValueChanges)
+            {
+                BreakerWeightBeforeHeat.ValueChanges = CalculateFormula;
+            }
+
+            if (null == BreakerWeightAfterHeat) BreakerWeightAfterHeat = new NRTestProperty();
+            BreakerWeightAfterHeat.LotNo = LotNo;
+            BreakerWeightAfterHeat.PropertyNo = PropertyNo;
+            BreakerWeightAfterHeat.NoOfSample = NoOfSample;
+            BreakerWeightAfterHeat.NeedSP = NeedSP;
+            // Check calculate action
+            if (null == BreakerWeightAfterHeat.ValueChanges)
+            {
+                BreakerWeightAfterHeat.ValueChanges = CalculateFormula;
+            }
+
+            if (null == RPU) RPU = new NRTestProperty();
+            RPU.LotNo = LotNo;
+            RPU.PropertyNo = PropertyNo;
+            RPU.NoOfSample = NoOfSample;
+            RPU.NeedSP = NeedSP;
+
+            CalculateFormula(); // calculate
+
+            this.Raise(() => this.EnableTest);
+        }
+
         #endregion
 
         #region Public Properties
 
-        #region BreakerW/BreakerWBH/BreakerWAH
+        #region LotNo/PropertyNo/NoOfSample
+
+        /// <summary>Gets or sets Lot No.</summary>
+        public string LotNo
+        {
+            get { return Get<string>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+        /// <summary>Gets or sets Property No.</summary>
+        public int PropertyNo
+        {
+            get { return Get<int>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+        /// <summary>Gets Max No of Test/Retest.</summary>
+        public int NoOfSample
+        {
+            get { return Get<int>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+        /// <summary>Gets or sets Need SP to Enable Test.</summary>
+        public bool NeedSP
+        {
+            get { return Get<bool>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    UpdateProperties();
+                });
+            }
+        }
+
+        #endregion
+
+        #region Spec
+
+        /// <summary>Gets or sets CordTestSpec.</summary>
+        public CordTestSpec Spec { get; set; }
+
+        #endregion
+
+        #region Enable Test (Normal/Re Test)
+
+        public bool EnableTest
+        {
+            get { return true; }
+            set { }
+        }
+
+        #endregion
+
+        #region BreakerWeight/BreakerWeightBeforeHeat/BreakerWeightAfterHeat/RPU
 
         /// <summary>Gets or sets BreakerWeight.</summary>
         public NRTestProperty BreakerWeight { get; set; }
@@ -66,6 +185,17 @@ namespace M3.QA.Models
         public NRTestProperty BreakerWeightBeforeHeat { get; set; }
         /// <summary>Gets or sets BreakerWeightAfterHeat.</summary>
         public NRTestProperty BreakerWeightAfterHeat { get; set; }
+        /// <summary>Gets or sets RPU.</summary>
+        public NRTestProperty RPU { get; set; }
+
+        #endregion
+
+        #region User/EditDate
+
+        public string InputBy { get; set; }
+        public DateTime? InputDate { get; set; }
+        public string EditBy { get; set; }
+        public DateTime? EditDate { get; set; }
 
         #endregion
 
