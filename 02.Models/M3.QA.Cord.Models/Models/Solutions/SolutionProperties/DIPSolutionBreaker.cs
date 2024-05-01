9d125ee8720b676b351789fbb1f9b8490cdd67e3
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-01 23:38:44 +0700

    M3 QA Cord #240 - Implements Cord Test Data #200   - Implements DIP Solution Test Data #12

--- 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Solutions/SolutionProperties/DIPSolutionBreaker.cs
@@ -25,7 +25,51 @@ namespace M3.QA.Models
     /// </summary>
     public class DIPSolutionBreaker : NInpc
     {
+        #region Constructor
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DIPSolutionBreaker() : base()
+        {
+            BreakerWeight = new NRTestProperty();
+            // Set calc formula
+            BreakerWeight.ValueChanges = CalculateFormula;
+
+            BreakerWeightBeforeHeat = new NRTestProperty();
+            // Set calc formula
+            BreakerWeight.ValueChanges = CalculateFormula;
+
+            BreakerWeightAfterHeat = new NRTestProperty();
+            // Set calc formula
+            BreakerWeight.ValueChanges = CalculateFormula;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void CalculateFormula()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #region BreakerW/BreakerWBH/BreakerWAH
+
+        /// <summary>Gets or sets BreakerWeight.</summary>
+        public NRTestProperty BreakerWeight { get; set; }
+        /// <summary>Gets or sets BreakerWeightBeforeHeat.</summary>
+        public NRTestProperty BreakerWeightBeforeHeat { get; set; }
+        /// <summary>Gets or sets BreakerWeightAfterHeat.</summary>
+        public NRTestProperty BreakerWeightAfterHeat { get; set; }
+
+        #endregion
+
+        #endregion
     }
 
     #endregion
