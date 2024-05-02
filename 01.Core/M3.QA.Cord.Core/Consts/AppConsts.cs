55bafb7fc53a15da2970613594809946664d39e5
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-02 14:19:40 +0700

    M3 QA Cord #257 - Implements Cord Test Data #217   - Update App build 257

--- 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.QA
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "255";
-                    public static DateTime LastUpdate = new DateTime(2024, 5, 2, 12, 30, 00);
+                    public static string Build = "257";
+                    public static DateTime LastUpdate = new DateTime(2024, 5, 2, 13, 30, 00);
                 }
 
                 public static class ExcelTest
