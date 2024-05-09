4478e08e49b96198d373d16ef67ae11031ae0408
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-09 10:39:46 +0700

    M3 QA Cord #265 - Implements Cord Test Data #225   - Implements Cord Production #1

---
+++ 05.Controls/M3.QA.Controls/Pages/CordProductionPage.xaml.cs
@@ -0,0 +1,119 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+#endregion
+
+namespace M3.QA.Pages
+{
+    /// <summary>
+    /// Interaction logic for CordProductionPage.xaml
+    /// </summary>
+    public partial class CordProductionPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordProductionPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Button Handlers
+
+        private void cmdBack_Click(object sender, RoutedEventArgs e)
+        {
+            M3QAApp.Pages.GotoQAMainMenu();
+        }
+
+        private void cmdSeach_Click(object sender, RoutedEventArgs e)
+        {
+            Search();
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e)
+        {
+            ClearSearch();
+        }
+
+        #endregion
+
+        #region TextBox Handlers
+
+        private void txtLotNo_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Search();
+                e.Handled = true;
+            }
+            else if (e.Key == Key.Escape)
+            {
+                ClearSearch();
+                e.Handled = true;
+            }
+        }
+
+        #endregion
+
+        #region DateTimePicker Handlers
+
+        private void dtDateFrom_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Search();
+        }
+
+        private void dtDateTo_ValueChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            Search();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Search()
+        {
+
+        }
+
+        private void ClearSearch()
+        {
+            txtLotNo.Text = string.Empty;
+            dtDateTo.Value = DateTime.Today;
+            dtDateFrom.Value = DateTime.Today;
+
+            grid.ItemsSource = null;
+            //searchs = new List<Models.Utils.P_SearchReceiveCord>();
+            //grid.ItemsSource = searchs;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Setup()
+        {
+            ClearSearch();
+        }
+
+        #endregion
+    }
+}
