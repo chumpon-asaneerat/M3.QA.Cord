5c0c89110b4372b608167b47b666d0d522a92458
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-09 11:47:31 +0700

    M3 QA Cord #269 - Implements Cord Test Data #229   - Implements Cord Production #5

---
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordProductionTestItem.cs
@@ -0,0 +1,395 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+using Dapper;
+using NLib;
+using NLib.Models;
+
+#endregion
+
+namespace M3.QA.Models
+{
+    public class CordProductionTest : NInpc
+    {
+        #region Internal Variables
+
+        private Func<int?> _GetSPNo;
+        private List<Func<decimal?>> _GetNs;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public CordProductionTest() : base()
+        {
+            #region Init Get/Set link methods
+
+            // Get SPNo
+            _GetSPNo = () => { return this.SPNo; };
+            // Get N
+            _GetNs = new List<Func<decimal?>>()
+            {
+                () => { return this.N1; },
+                () => { return this.N2; },
+                () => { return this.N3; },
+                () => { return this.N4; },
+                () => { return this.N5; },
+                () => { return this.N6; },
+                () => { return this.N7; }
+            };
+
+            #endregion
+
+            BuildItems(0); // create empty items.
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void BuildItems(int noOfSample)
+        {
+            lock (this)
+            {
+                Items = new List<CordProductionTestItem>();
+                CordProductionTestItem item;
+                for (int i = 1; i <= 7; i++)
+                {
+                    if (i > noOfSample) continue; // skip if more than allow no of sample.
+
+                    item = new CordProductionTestItem();
+                    // set Sample No.
+                    item.No = i;
+                    // assign method pointer to Get SPNo/Need SP
+                    item.GetSPNo = (null != _GetSPNo) ? _GetSPNo : null;
+                    // assign method pointer to Get/Set N
+                    item.GetN = (null != _GetNs) ? _GetNs[i - 1] : null;
+
+                    Items.Add(item);
+                }
+            }
+        }
+
+        private void ValueChange([CallerMemberName] string propertyName = "")
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+                return;
+            if (null == this.Items)
+                return; // No items.
+
+            if (propertyName.StartsWith("N"))
+            {
+                string sIdx = propertyName.Replace("N", string.Empty);
+                int idx;
+                if (int.TryParse(sIdx, out idx))
+                {
+                    // Note: N1 -> index must be 0, N2  -> index must be 1 so need decrease index by 1.
+                    idx--; // remove by 1 for zero based
+
+                    if (idx < 0 || idx >= this.Items.Count) return;
+                    this.Items[idx].RaiseNChanges();
+                    CheckSpec();
+                    CalcAvg();
+                }
+            }
+            else if (propertyName.StartsWith("SPNo"))
+            {
+                lock (this)
+                {
+                    foreach (var item in Items)
+                    {
+                        item.RaiseSPNoChanges();
+                    }
+                }
+            }
+        }
+
+        private void CalcAvg()
+        {
+            decimal total = decimal.Zero;
+            int iCnt = 0;
+            lock (this)
+            {
+                if (null != this.Items)
+                {
+                    foreach (var item in this.Items)
+                    {
+                        if (item.N.HasValue)
+                        {
+                            // Has N value and no R value so use N to calc avg
+                            total += item.N.Value;
+                            ++iCnt;
+                        }
+                    }
+                }
+            }
+            // Calc average value.
+            this.Avg = (iCnt > 0) ? (total / iCnt) : new decimal?();
+
+            if (null != ValueChanges) ValueChanges();
+        }
+
+        #endregion
+
+        #region Virtual Methods
+
+        /// <summary>
+        /// Check Spec.
+        /// </summary>
+        protected virtual void CheckSpec()
+        {
+
+        }
+
+        #endregion
+
+        #region Callback Actions
+
+        internal Action ValueChanges { get; set; }
+
+        #endregion
+
+        #region Public Properties
+
+        #region LotNo/PropertyNo/SPNo/NoOfSample
+
+        /// <summary>Gets or sets Lot No.</summary>
+        public string LotNo { get; set; }
+        /// <summary>Gets or sets Property No.</summary>
+        public int PropertyNo { get; set; }
+        /// <summary>Gets or sets SP No.</summary>
+        public int? SPNo
+        {
+            get { return Get<int?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets Max No of Test/Retest.</summary>
+        public int NoOfSample
+        {
+            get { return (null != Items) ? Items.Count : 0; }
+            set
+            {
+                BuildItems(value);
+                // Raise events
+                this.Raise(() => this.NoOfSample);
+                this.Raise(() => this.Items);
+            }
+        }
+        /// <summary>Gets or sets Yarn Type.</summary>
+        public string YarnType
+        {
+            get { return Get<string>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+
+        #endregion
+
+        #region Spec
+
+        /// <summary>Gets or sets CordTestSpec.</summary>
+        public CordTestSpec Spec { get; set; }
+
+        #endregion
+
+        #region Normal Test (1-7)
+
+        /// <summary>Gets or sets N1 value.</summary>
+        public decimal? N1
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets or sets N2 value.</summary>
+        public decimal? N2
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets or sets N3 value.</summary>
+        public decimal? N3
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets or sets N4 value.</summary>
+        public decimal? N4
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets or sets N5 value.</summary>
+        public decimal? N5
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets or sets N6 value.</summary>
+        public decimal? N6
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+        /// <summary>Gets or sets N7 value.</summary>
+        public decimal? N7
+        {
+            get { return Get<decimal?>(); }
+            set
+            {
+                Set(value, () =>
+                {
+                    ValueChange();
+                });
+            }
+        }
+
+        #endregion
+
+        #region Avg
+
+        /// <summary>Gets or sets Avg value.</summary>
+        public decimal? Avg
+        {
+            get { return Get<decimal?>(); }
+            set { Set(value, () => { }); }
+        }
+
+        #endregion
+
+        #region Items
+
+        /// <summary>
+        /// Gets N Items.
+        /// </summary>
+        public List<CordProductionTestItem> Items { get; set; }
+
+        #endregion
+
+        #endregion
+    }
+
+    public class CordProductionTestItem : NInpc
+    {
+        #region Protected Properties
+
+        // SP Gets
+        protected internal Func<int?> GetSPNo { get; set; }
+        // N Gets
+        protected internal Func<decimal?> GetN { get; set; }
+
+        #endregion
+
+        #region virtual methods
+
+        protected internal void RaiseSPNoChanges()
+        {
+            // Raise relelated events
+            Raise(() => this.SPNo);
+        }
+
+        protected internal void RaiseNChanges()
+        {
+            // Raise relelated events
+            Raise(() => this.N);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        #region SP/No
+
+        /// <summary>
+        /// Gets or sets SP No.
+        /// </summary>
+        public int? SPNo
+        {
+            get { return (null != GetSPNo) ? GetSPNo() : new int?(); }
+            set { }
+        }
+        /// <summary>Gets or sets Test No. (N1, N2, N3)</summary>
+        public int No { get; set; }
+
+        #endregion
+
+        #region N
+
+        /// <summary>Gets or sets Test Value.</summary>
+        public decimal? N
+        {
+            get { return (null != GetN) ? GetN() : new decimal?(); }
+            set { }
+        }
+
+        #endregion
+
+        #region CaptionN (For Runtime binding)
+
+        /// <summary>Gets N Display Caption.</summary>
+        public string CaptionN
+        {
+            get { return "N" + No.ToString(); }
+            set { }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
