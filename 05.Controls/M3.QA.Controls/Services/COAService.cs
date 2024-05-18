bfd887f71ca0d4608954edbae404be16fa2b1eea
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-18 22:40:39 +0700

    M3 QA Cord #287 - Implements Cord Test Data #247   - Add COA5 service class.

--- 05.Controls/M3.QA.Controls/Services/COAService.cs
+++ 05.Controls/M3.QA.Controls/Services/COAService.cs
@@ -146,12 +146,33 @@ namespace M3.QA
                     ws.Cells["C" + iRow.ToString()].Value = "(" + p.Spec.UnitReport + ")";
                     // SPEC
                     ws.Cells["D" + iRow.ToString()].Value = p.Spec.ReportSpec;
-                    // RESULT
-                    ws.Cells["E" + iRow.ToString()].Value = p.Avg;
-                    // JUDGE
-                    ws.Cells["G" + iRow.ToString()].Value = p.Spec.IsOutOfSpec(p.Avg) ? "NG" : "OK";
-                    // Test Method
-                    ws.Cells["H" + iRow.ToString()].Value = p.Spec.TestMethod;
+
+                    // RESULT 1-5
+                    int iCnt = 1;
+                    foreach (var test in p.Tests)
+                    {
+                        if (iCnt == 1)
+                        {
+                            ws.Cells["G" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 2)
+                        {
+                            ws.Cells["H" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 3)
+                        {
+                            ws.Cells["I" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 4)
+                        {
+                            ws.Cells["J" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 5)
+                        {
+                            ws.Cells["K" + iRow.ToString()].Value = test.Avg;
+                        }
+                        iCnt++;
+                    }
                 }
             }
 
@@ -187,20 +208,24 @@ namespace M3.QA
                             #region Write Cells
                             /*
                             // DATE
-                            ws.Cells["I7"].Value = (value.InputDate.HasValue) ?
+                            ws.Cells["M7"].Value = (value.InputDate.HasValue) ?
                                 value.InputDate.Value.Date : new DateTime?();
                             // USER
-                            ws.Cells["B11"].Value = value.UserName;
-                            // PRODUCT
-                            ws.Cells["B12"].Value = value.ProductName;
+                            ws.Cells["B10"].Value = value.UserName;
                             // ITEM CODE
-                            ws.Cells["B13"].Value = value.ItemCode;
-                            // YARN TYPE
-                            ws.Cells["B14"].Value = value.YarnCode;
+                            ws.Cells["I10"].Value = value.ItemCode;
                             // LOT NO
-                            ws.Cells["B15"].Value = value.LotNo;
-                            // PI NO 
-                            ws.Cells["B16"].Value = value.PiNoSL;
+                            ws.Cells["N10"].Value = value.LotNo;
+                            */
+                            #endregion
+
+                            #region Write Lot 1-5
+                            /*
+                            ws.Cells["G14"].Value = string.Format("{0}-1", value.LotNo);
+                            ws.Cells["H14"].Value = string.Format("{0}-2", value.LotNo);
+                            ws.Cells["I14"].Value = string.Format("{0}-3", value.LotNo);
+                            ws.Cells["J14"].Value = string.Format("{0}-4", value.LotNo);
+                            ws.Cells["K14"].Value = string.Format("{0}-5", value.LotNo);
                             */
                             #endregion
 
@@ -209,34 +234,31 @@ namespace M3.QA
                             CordProductionProperty p;
                             // TENSILE STRENGTH (PropertyNo = 1)
                             p = value.Properties.FindByPropertyNo(1);
-                            WriteProperty(ws, 20, p);
+                            WriteProperty(ws, 15, p);
                             // ELONG AT BREAK (PropertyNo = 2)
                             p = value.Properties.FindByPropertyNo(2);
-                            WriteProperty(ws, 21, p);
+                            WriteProperty(ws, 16, p);
                             // ELONG AT LOAD (PropertyNo = 3)
                             p = value.Properties.FindByPropertyNo(3);
-                            WriteProperty(ws, 22, p);
-                            // NO OF TWIST (PropertyNo = 7)
+                            WriteProperty(ws, 17, p);
+                            // FIRST TWIST (PropertyNo = 7)
                             p = value.Properties.FindByPropertyNo(7);
-                            WriteProperty(ws, 23, p);
-                            // CORD GAUGE (PropertyNo = 9)
-                            p = value.Properties.FindByPropertyNo(9);
-                            WriteProperty(ws, 24, p);
+                            WriteProperty(ws, 18, p);
+                            // SECOND TWIST (PropertyNo = 8)
+                            p = value.Properties.FindByPropertyNo(8);
+                            WriteProperty(ws, 19, p);
                             // THERMAL SHRINKAGE (PropertyNo = 6)
                             p = value.Properties.FindByPropertyNo(6);
-                            WriteProperty(ws, 25, p);
-                            // CORD SIZE (PropertyNo = 10)
-                            p = value.Properties.FindByPropertyNo(10);
-                            WriteProperty(ws, 26, p);
+                            WriteProperty(ws, 20, p);
+                            // SHRINKAGE FORCE (PropertyNo = 5)
+                            p = value.Properties.FindByPropertyNo(5);
+                            WriteProperty(ws, 21, p);
                             // MOISTURE REGAIN (PropertyNo = 11)
                             p = value.Properties.FindByPropertyNo(11);
-                            WriteProperty(ws, 27, p);
-                            // RPU (PropertyNo = 12)
-                            p = value.Properties.FindByPropertyNo(12);
-                            WriteProperty(ws, 28, p);
-                            // ADHESION FORCE (PEEL) (PropertyNo = 4)
-                            p = value.Properties.FindByPropertyNo(4);
-                            WriteProperty(ws, 29, p);
+                            WriteProperty(ws, 22, p);
+                            // FINENESS (PropertyNo = 10)
+                            p = value.Properties.FindByPropertyNo(10);
+                            WriteProperty(ws, 23, p);
                             */
                             #endregion
                         }
@@ -267,12 +289,33 @@ namespace M3.QA
                     ws.Cells["C" + iRow.ToString()].Value = "(" + p.Spec.UnitReport + ")";
                     // SPEC
                     ws.Cells["D" + iRow.ToString()].Value = p.Spec.ReportSpec;
-                    // RESULT
-                    ws.Cells["E" + iRow.ToString()].Value = p.Avg;
-                    // JUDGE
-                    ws.Cells["G" + iRow.ToString()].Value = p.Spec.IsOutOfSpec(p.Avg) ? "NG" : "OK";
-                    // Test Method
-                    ws.Cells["H" + iRow.ToString()].Value = p.Spec.TestMethod;
+
+                    // RESULT 1-5
+                    int iCnt = 1;
+                    foreach (var test in p.Tests)
+                    {
+                        if (iCnt == 1)
+                        {
+                            ws.Cells["G" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 2)
+                        {
+                            ws.Cells["H" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 3)
+                        {
+                            ws.Cells["I" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 4)
+                        {
+                            ws.Cells["J" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 5)
+                        {
+                            ws.Cells["K" + iRow.ToString()].Value = test.Avg;
+                        }
+                        iCnt++;
+                    }
                 }
             }
 
@@ -308,20 +351,24 @@ namespace M3.QA
                             #region Write Cells
                             /*
                             // DATE
-                            ws.Cells["I7"].Value = (value.InputDate.HasValue) ?
+                            ws.Cells["M7"].Value = (value.InputDate.HasValue) ?
                                 value.InputDate.Value.Date : new DateTime?();
                             // USER
-                            ws.Cells["B11"].Value = value.UserName;
-                            // PRODUCT
-                            ws.Cells["B12"].Value = value.ProductName;
+                            ws.Cells["B10"].Value = value.UserName;
                             // ITEM CODE
-                            ws.Cells["B13"].Value = value.ItemCode;
-                            // YARN TYPE
-                            ws.Cells["B14"].Value = value.YarnCode;
+                            ws.Cells["I10"].Value = value.ItemCode;
                             // LOT NO
-                            ws.Cells["B15"].Value = value.LotNo;
-                            // PI NO 
-                            ws.Cells["B16"].Value = value.PiNoSL;
+                            ws.Cells["N10"].Value = value.LotNo;
+                            */
+                            #endregion
+
+                            #region Write Lot 1-5
+                            /*
+                            ws.Cells["G14"].Value = string.Format("{0}-1", value.LotNo);
+                            ws.Cells["H14"].Value = string.Format("{0}-2", value.LotNo);
+                            ws.Cells["I14"].Value = string.Format("{0}-3", value.LotNo);
+                            ws.Cells["J14"].Value = string.Format("{0}-4", value.LotNo);
+                            ws.Cells["K14"].Value = string.Format("{0}-5", value.LotNo);
                             */
                             #endregion
 
@@ -330,34 +377,31 @@ namespace M3.QA
                             CordProductionProperty p;
                             // TENSILE STRENGTH (PropertyNo = 1)
                             p = value.Properties.FindByPropertyNo(1);
-                            WriteProperty(ws, 20, p);
+                            WriteProperty(ws, 15, p);
                             // ELONG AT BREAK (PropertyNo = 2)
                             p = value.Properties.FindByPropertyNo(2);
-                            WriteProperty(ws, 21, p);
+                            WriteProperty(ws, 16, p);
                             // ELONG AT LOAD (PropertyNo = 3)
                             p = value.Properties.FindByPropertyNo(3);
-                            WriteProperty(ws, 22, p);
-                            // NO OF TWIST (PropertyNo = 7)
+                            WriteProperty(ws, 17, p);
+                            // FIRST TWIST (PropertyNo = 7)
                             p = value.Properties.FindByPropertyNo(7);
-                            WriteProperty(ws, 23, p);
-                            // CORD GAUGE (PropertyNo = 9)
-                            p = value.Properties.FindByPropertyNo(9);
-                            WriteProperty(ws, 24, p);
+                            WriteProperty(ws, 18, p);
+                            // SECOND TWIST (PropertyNo = 8)
+                            p = value.Properties.FindByPropertyNo(8);
+                            WriteProperty(ws, 19, p);
                             // THERMAL SHRINKAGE (PropertyNo = 6)
                             p = value.Properties.FindByPropertyNo(6);
-                            WriteProperty(ws, 25, p);
-                            // CORD SIZE (PropertyNo = 10)
-                            p = value.Properties.FindByPropertyNo(10);
-                            WriteProperty(ws, 26, p);
+                            WriteProperty(ws, 20, p);
+                            // SHRINKAGE FORCE (PropertyNo = 5)
+                            p = value.Properties.FindByPropertyNo(5);
+                            WriteProperty(ws, 21, p);
                             // MOISTURE REGAIN (PropertyNo = 11)
                             p = value.Properties.FindByPropertyNo(11);
-                            WriteProperty(ws, 27, p);
-                            // RPU (PropertyNo = 12)
-                            p = value.Properties.FindByPropertyNo(12);
-                            WriteProperty(ws, 28, p);
-                            // ADHESION FORCE (PEEL) (PropertyNo = 4)
-                            p = value.Properties.FindByPropertyNo(4);
-                            WriteProperty(ws, 29, p);
+                            WriteProperty(ws, 22, p);
+                            // FINENESS (PropertyNo = 10)
+                            p = value.Properties.FindByPropertyNo(10);
+                            WriteProperty(ws, 23, p);
                             */
                             #endregion
                         }
@@ -502,6 +546,149 @@ namespace M3.QA
                             p = value.Properties.FindByPropertyNo(10);
                             WriteProperty(ws, 23, p);
 
+                            #endregion
+                        }
+
+                        package.Save();
+                    }
+                    M3QAApp.Windows.ExportSuccess();
+                }
+                catch (Exception ex)
+                {
+                    med.Err(ex);
+                    M3QAApp.Windows.ExportFailed(ex.Message);
+                }
+            }
+        }
+
+        #endregion
+
+        #region COA 5
+
+        public class COA5
+        {
+            private static void WriteProperty(ExcelWorksheet ws, int iRow, CordProductionProperty p)
+            {
+                if (null != ws && null != p && null != p.Spec)
+                {
+                    // Unit Report
+                    ws.Cells["C" + iRow.ToString()].Value = "(" + p.Spec.UnitReport + ")";
+                    // SPEC
+                    ws.Cells["D" + iRow.ToString()].Value = p.Spec.ReportSpec;
+
+                    // RESULT 1-5
+                    int iCnt = 1;
+                    foreach (var test in p.Tests)
+                    {
+                        if (iCnt == 1)
+                        {
+                            ws.Cells["G" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 2)
+                        {
+                            ws.Cells["H" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 3)
+                        {
+                            ws.Cells["I" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 4)
+                        {
+                            ws.Cells["J" + iRow.ToString()].Value = test.Avg;
+                        }
+                        else if (iCnt == 5)
+                        {
+                            ws.Cells["K" + iRow.ToString()].Value = test.Avg;
+                        }
+                        iCnt++;
+                    }
+                }
+            }
+
+            public static void Export(CordProduction value)
+            {
+                MethodBase med = MethodBase.GetCurrentMethod();
+
+                string outputFile = ExcelModel.Dialogs.SaveDialog();
+                if (string.IsNullOrEmpty(outputFile))
+                    return;
+
+                if (null == value)
+                    return;
+
+                if (null != value)
+                {
+                    value.LoadProperties();
+                }
+
+                if (!ExcelExportUtils.CreateCOA5File(outputFile, true))
+                {
+                    M3QAApp.Windows.ExportFailed();
+                    return;
+                }
+
+                try
+                {
+                    using (var package = new ExcelPackage(outputFile))
+                    {
+                        var ws = package.Workbook.Worksheets[0]; // check exists
+                        if (null != ws)
+                        {
+                            #region Write Cells
+                            /*
+                            // DATE
+                            ws.Cells["M7"].Value = (value.InputDate.HasValue) ?
+                                value.InputDate.Value.Date : new DateTime?();
+                            // USER
+                            ws.Cells["B10"].Value = value.UserName;
+                            // ITEM CODE
+                            ws.Cells["I10"].Value = value.ItemCode;
+                            // LOT NO
+                            ws.Cells["N10"].Value = value.LotNo;
+                            */
+                            #endregion
+
+                            #region Write Lot 1-5
+                            /*
+                            ws.Cells["G14"].Value = string.Format("{0}-1", value.LotNo);
+                            ws.Cells["H14"].Value = string.Format("{0}-2", value.LotNo);
+                            ws.Cells["I14"].Value = string.Format("{0}-3", value.LotNo);
+                            ws.Cells["J14"].Value = string.Format("{0}-4", value.LotNo);
+                            ws.Cells["K14"].Value = string.Format("{0}-5", value.LotNo);
+                            */
+                            #endregion
+
+                            #region Write each properties
+                            /*
+                            CordProductionProperty p;
+                            // TENSILE STRENGTH (PropertyNo = 1)
+                            p = value.Properties.FindByPropertyNo(1);
+                            WriteProperty(ws, 15, p);
+                            // ELONG AT BREAK (PropertyNo = 2)
+                            p = value.Properties.FindByPropertyNo(2);
+                            WriteProperty(ws, 16, p);
+                            // ELONG AT LOAD (PropertyNo = 3)
+                            p = value.Properties.FindByPropertyNo(3);
+                            WriteProperty(ws, 17, p);
+                            // FIRST TWIST (PropertyNo = 7)
+                            p = value.Properties.FindByPropertyNo(7);
+                            WriteProperty(ws, 18, p);
+                            // SECOND TWIST (PropertyNo = 8)
+                            p = value.Properties.FindByPropertyNo(8);
+                            WriteProperty(ws, 19, p);
+                            // THERMAL SHRINKAGE (PropertyNo = 6)
+                            p = value.Properties.FindByPropertyNo(6);
+                            WriteProperty(ws, 20, p);
+                            // SHRINKAGE FORCE (PropertyNo = 5)
+                            p = value.Properties.FindByPropertyNo(5);
+                            WriteProperty(ws, 21, p);
+                            // MOISTURE REGAIN (PropertyNo = 11)
+                            p = value.Properties.FindByPropertyNo(11);
+                            WriteProperty(ws, 22, p);
+                            // FINENESS (PropertyNo = 10)
+                            p = value.Properties.FindByPropertyNo(10);
+                            WriteProperty(ws, 23, p);
+                            */
                             #endregion
                         }
 
