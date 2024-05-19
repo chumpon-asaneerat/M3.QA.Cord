d15c999eb901787702cd159ef7c035ef19f1d06f
chumpon-asaneerat <chumponsenate@gmail.com>
2024-05-19 14:57:38 +0700

    M3 QA Cord #299 - Implements Cord Test Data #259   - Implements DIP Solution Production #1

---
+++ 05.Controls/M3.QA.Controls/Pages/DIPSolutionProductionPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿#region Using
+
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+using NLib;
+using NLib.Models;
+using M3.QA.Models;
+
+#endregion
+
+namespace M3.QA.Pages
+{
+    /// <summary>
+    /// Interaction logic for DIPSolutionProductionPage.xaml
+    /// </summary>
+    public partial class DIPSolutionProductionPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DIPSolutionProductionPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
