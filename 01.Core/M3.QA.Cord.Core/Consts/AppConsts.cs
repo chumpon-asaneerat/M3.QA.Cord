663ae8eb049dce72f0b8b6dd3a5af62aba162b0f
chumpon-asaneerat <chumponsenate@gmail.com>
2024-06-04 07:38:17 +0700

    M3 QA Cord #346 - Implements Cord Test Data #306   - Re-commit Update App build 345

--- 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
@@ -24,7 +24,7 @@ namespace M3.QA
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
                     public static string Build = "345";
-                    public static DateTime LastUpdate = new DateTime(2024, 6, 4, 4, 40, 00);
+                    public static DateTime LastUpdate = new DateTime(2024, 6, 4, 7, 40, 00);
                 }
 
                 public static class ExcelTest
