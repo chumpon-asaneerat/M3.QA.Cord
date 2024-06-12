1cfeecb1e80d4be37d012d35af6b853e3518baa1
chumpon-asaneerat <chumponsenate@gmail.com>
2024-06-13 06:08:05 +0700

    M3 QA Cord #388 - Implements Cord Test Data #318   - Implements Excel import data with retest #3

--- 02.Models/M3.QA.Cord.Models/Models/Excels/UnitTestSPParsers.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Excels/UnitTestSPParsers.cs
@@ -25,12 +25,77 @@ namespace M3.QA.Models
     /// </summary>
     public class UniTestSPParserItem
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private UniTestSPParserItem() : base() 
+        {
+            NCnt = 3;
+            RCnt = 0;
+        }
+
+        #endregion
+
         #region Public Properties
 
         /// <summary>Gets or sets SP No.</summary>
         public int SP { get; set; }
+        /// <summary>Gets or sets Test Count.</summary>
+        public int NCnt { get; set; }
         /// <summary>Gets or sets Retest Count.</summary>
         public int RCnt { get; set; }
+
+        #endregion
+
+        #region Static Methods
+
+        public static UniTestSPParserItem Parse(string value)
+        {
+            UniTestSPParserItem inst = null;
+            if (string.IsNullOrWhiteSpace(value))
+                return inst;
+
+            string sSP = string.Empty;
+            string sRCnt = string.Empty;
+            if (value.Contains("+") || value.Contains("/"))
+            {
+                var lines = value.Split(new string[] { "+", "/" }, StringSplitOptions.RemoveEmptyEntries);
+                if (null != lines && lines.Length > 0) 
+                { 
+                    sSP = lines[0]; // first element is SP No.
+                    if (lines.Length > 1)
+                    {
+                        // Has retest
+                        sRCnt = lines[1];
+                    }
+                    else
+                    {
+                        sRCnt = "0";
+                    }
+                }
+            }
+            else
+            {
+                sSP = value;
+                sRCnt = "0";
+            }
+            int sp, rcnt;
+            if (int.TryParse(sSP, out sp))
+            {
+                // parse success.
+                inst = new UniTestSPParserItem();
+                inst.SP = sp;
+                if (int.TryParse(sRCnt, out rcnt))
+                {
+                    inst.RCnt = rcnt;
+                }
+                else inst.RCnt = 0;
+            }
+
+            return inst;
+        }
 
         #endregion
     }
@@ -71,7 +136,18 @@ namespace M3.QA.Models
             if (string.IsNullOrWhiteSpace(value)) 
                 return inst;
 
+            string[] sps = value.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+            int noOfSP = (null != sps) ? sps.Length : 0;
+
             inst = new UniTestSPParser();
+
+            UniTestSPParserItem item;
+            for (int i = 0; i < noOfSP; i++)
+            {
+                item = UniTestSPParserItem.Parse(sps[i]);
+                if (null != item) 
+                    inst.Items.Add(item); // append to items list.
+            }
 
             return inst;
         }
