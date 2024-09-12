df83e53f1ca5c0a7b15c8725ad7ef143b014b887
chumpon-asaneerat <chumponsenate@gmail.com>
2024-09-12 12:07:50 +0700

    M3 QA Cord #476 - Implements Cord Test Data #66

--- 02.Models/M3.QA.Cord.Models/Models/Cords/CordProperties/CordShrinkageForce.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordProperties/CordShrinkageForce.cs
@@ -152,40 +152,40 @@ namespace M3.QA.Models
                     break; // already reach max allow SP
 
                 int? SP;
-                //string sampleType;
+                string sampleType;
                 switch (i)
                 {
                     case 1:
                         SP = value.SP1;
-                        //sampleType = value.SampleType1;
+                        sampleType = value.SampleType1;
                         break;
                     case 2:
                         SP = value.SP2;
-                        //sampleType = value.SampleType2;
+                        sampleType = value.SampleType2;
                         break;
                     case 3:
                         SP = value.SP3;
-                        //sampleType = value.SampleType3;
+                        sampleType = value.SampleType3;
                         break;
                     case 4:
                         SP = value.SP4;
-                        //sampleType = value.SampleType4;
+                        sampleType = value.SampleType4;
                         break;
                     case 5:
                         SP = value.SP5;
-                        //sampleType = value.SampleType5;
+                        sampleType = value.SampleType5;
                         break;
                     case 6:
                         SP = value.SP6;
-                        //sampleType = value.SampleType6;
+                        sampleType = value.SampleType6;
                         break;
                     case 7:
                         SP = value.SP7;
-                        //sampleType = value.SampleType7;
+                        sampleType = value.SampleType7;
                         break;
                     default:
                         SP = new int?();
-                        //sampleType = null;
+                        sampleType = null;
                         break;
                 }
                 // Skip SP is null
@@ -200,7 +200,7 @@ namespace M3.QA.Models
                     LotNo = value.LotNo,
                     PropertyNo = 5, // Shrinkage Force = 5
                     SPNo = SP,
-                    //SampleType = sampleType,
+                    SampleType = sampleType,
                     NeedSP = true,
                     Spec = spec,
                     YarnType = value.YarnType,
@@ -225,7 +225,7 @@ namespace M3.QA.Models
                         // need to set because not return from db.
                         existItems[idx].NoOfSample = item.NoOfSample;
                         existItems[idx].YarnType = item.YarnType;
-                        //existItems[idx].SampleType = item.SampleType;
+                        existItems[idx].SampleType = item.SampleType;
                         // Clone anther properties
                         Clone(existItems[idx], item);
                     }
