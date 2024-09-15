dc8bee8ae2a9ef66db017ebaf56afa14feac382a
chumpon-asaneerat <chumponsenate@gmail.com>
2024-09-15 19:57:42 +0700

    M3 QA Cord #509 - Implements Cord Test Data #99   - Recompile all projects.

--- 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.QA
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "507";
-                    public static DateTime LastUpdate = new DateTime(2024, 9, 15, 19, 40, 00);
+                    public static string Build = "510";
+                    public static DateTime LastUpdate = new DateTime(2024, 9, 15, 20, 00, 00);
                 }
 
                 public static class ExcelTest
