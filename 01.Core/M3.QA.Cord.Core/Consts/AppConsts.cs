41faff14775f5198df0d2c296527e50c1e1f2e5a
chumpon-asaneerat <chumponsenate@gmail.com>
2024-09-15 19:20:57 +0700

    M3 QA Cord #504 - Implements Cord Test Data #94   - Change app build 505

--- 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.QA
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "502";
-                    public static DateTime LastUpdate = new DateTime(2024, 9, 15, 15, 30, 00);
+                    public static string Build = "505";
+                    public static DateTime LastUpdate = new DateTime(2024, 9, 15, 19, 20, 00);
                 }
 
                 public static class ExcelTest
