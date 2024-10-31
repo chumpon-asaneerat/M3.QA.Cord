73d03ce83e26fe90822bd88179e0fb06ae4e3308
chumpon-asaneerat <chumponsenate@gmail.com>
2024-10-31 14:28:18 +0700

    M3 QA Cord #532 - Update app build 532

--- 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
+++ 01.Core/M3.QA.Cord.Core/Consts/AppConsts.cs
@@ -23,8 +23,8 @@ namespace M3.QA
                     // common
                     public static string Version = AppConsts.Version;
                     public static string Minor = AppConsts.Minor;
-                    public static string Build = "530";
-                    public static DateTime LastUpdate = new DateTime(2024, 10, 29, 15, 00, 00);
+                    public static string Build = "532";
+                    public static DateTime LastUpdate = new DateTime(2024, 10, 31, 14, 30, 00);
                 }
 
                 public static class ExcelTest
