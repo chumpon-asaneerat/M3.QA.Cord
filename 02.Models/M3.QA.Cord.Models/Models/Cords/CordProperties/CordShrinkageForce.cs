d0557093889e6a1d47b8435056a5338aa07e3893
chumpon-asaneerat <chumponsenate@gmail.com>
2025-05-24 13:56:19 +0700

    M3 QA Cord #558 - Fixed Cord Test Data ShinkageFroce not show N data

--- 02.Models/M3.QA.Cord.Models/Models/Cords/CordProperties/CordShrinkageForce.cs
+++ 02.Models/M3.QA.Cord.Models/Models/Cords/CordProperties/CordShrinkageForce.cs
@@ -221,7 +221,7 @@ namespace M3.QA.Models
                 {
                     idx = existItems.FindIndex((x) => 
                     { 
-                        return x.SPNo == item.SPNo && x.PropertyNo == item.PropertyNo && 
+                        return x.SPNo == item.SPNo && /* x.PropertyNo == item.PropertyNo && */
                             x.SampleType == item.SampleType; 
                     });
                     if (idx != -1)
